5da517d6489c2f68717f09258543d6d397fbbad5
deepparag <deep.p.sarda@gmail.com>
2022-05-11 22:45:13 +0530

    Clean up code

--- models/economy/equipped.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-let bal = mongoose.Schema({
-  user: { type: String, required: true },
-  itemEquipped: { type: String, default: "Default Shotgun" },
-});
-
-module.exports = mongoose.model("equippedItem", bal);
--- models/economy/items.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-let bal = mongoose.Schema({
-  user: { type: String },
-  Inventory: { type: Object },
-});
-
-module.exports = mongoose.model("ecoitems", bal);
