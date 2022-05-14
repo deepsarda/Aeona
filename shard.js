f39b5b71d462c1e1b7f87ac6ac124241a757d8e1
deepparag <deep.p.sarda@gmail.com>
2022-05-14 22:48:30 +0530

    Add auto shard restarter

--- shard.js
+++ shard.js
@@ -1,51 +1,56 @@
-
 process.on("uncaughtException", (err, origin) => {
-  console.log("An error occured"+err);
+  console.log("An error occured" + err);
 });
 
+console.log("starting");
+const dotenv = require("dotenv").config();
+const logger = require("./utils/logger");
+const Discord = require("discord.js");
+const { token } = require("./utils/variables.js");
+const { AutoPoster } = require("topgg-autoposter");
+const Statcord = require("statcord.js");
+const { Console } = require("console");
 
+const manager = new Discord.ShardingManager("./index.js", {
+  token: token,
+  //autoSpawn: true,
+  // totalShards: 'auto'
+  totalShards: 1,
+});
+const poster = AutoPoster(process.env.TOKEN, manager);
 
-    console.log("starting")
-    const dotenv = require("dotenv").config();
-    const logger = require("./utils/logger");
-    const Discord = require("discord.js");
-    const { token } = require("./utils/variables.js");
-    const { AutoPoster } = require("topgg-autoposter");
-    const Statcord = require("statcord.js");
+let statcord = new Statcord.ShardingClient({
+  manager,
+  key: process.env.STATCORD,
+});
 
-    const manager = new Discord.ShardingManager("./index.js", {
-      token: token,
-      //autoSpawn: true,
-      // totalShards: 'auto'
-      totalShards: 1,
-    });
-    const poster = AutoPoster(process.env.TOKEN, manager);
+statcord.on("autopost-start", () => {
+  // Emitted when statcord autopost starts
+  console.log("Started autopost");
+});
 
-    let statcord = new Statcord.ShardingClient({
-      manager,
-      key: process.env.STATCORD,
-    });
-
-    statcord.on("autopost-start", () => {
-      // Emitted when statcord autopost starts
-      console.log("Started autopost");
-    });
-
-    statcord.on("post", (status) => {
-      // status = false if the post was successful
-      // status = "Error message" or status = Error if there was an error
-      if (status)  console.error(status);
-    });
-
-    manager.spawn();
-    manager.on("shardCreate", (shard) => {
-      logger.info(`Launching Shard ${shard.id + 1}`, { label: `Shard` });
-      manager
-        .fetchClientValues("guilds.cache.size")
-        .then((results) =>
-          console.log(
-            `${results.reduce((prev, val) => prev + val, 0)} total guilds`
-          )
-        )
-        .catch(console.error);
-    });
+statcord.on("post", (status) => {
+  // status = false if the post was successful
+  // status = "Error message" or status = Error if there was an error
+  if (status) console.error(status);
+});
+while (true) {
+  try {
+    Console.log("Attempting to spawn shards");
+    await manager.spawn();
+    break;
+  } catch (e) {
+    console.log(e);
+  }
+}
+manager.on("shardCreate", (shard) => {
+  logger.info(`Launching Shard ${shard.id + 1}`, { label: `Shard` });
+  manager
+    .fetchClientValues("guilds.cache.size")
+    .then((results) =>
+      console.log(
+        `${results.reduce((prev, val) => prev + val, 0)} total guilds`
+      )
+    )
+    .catch(console.error);
+});
