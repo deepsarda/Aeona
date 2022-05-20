2fc5ef0a8caaa069adf2b71b53db145626575b96
deepsarda <92147339+deepsarda@users.noreply.github.com>
2022-05-20 21:22:44 +0530

    Update profile

--- commands/economy/profile.js
+++ commands/economy/profile.js
@@ -13,6 +13,22 @@ module.exports = class extends Command {
     });
   }
   async run(message, args) {
+    const mentionUser = message.mentions.members.first()&&message.mentions.members.filter(m=>args[0]&&args[0].includes(m.user.id)).size>=1?message.mentions.members.filter(m=>args[0]&&args[0].includes(m.user.id)).first():false|| message.guild.members.cache.get(args[0])|| args.length > 0 && message.guild.members.cache.find(m => m.user.username.toLowerCase().includes(args.join(" ").toLowerCase()))||message.member;
     
-  }
+    let user=message.client.economy.getUser(mentionUser.id)
+
+    let embed=new Discord.MessageEmbed().setTitle(`${user}'s Profile`).setThumbnail(user.getDisplayAvatar({dynamic:true})).addField("Wallet",user.money.wallet.toLocaleString()+'₪')
+    .addField(`Bank`,user.money.bank.toLocaleString()+'/'+user.money.maxBank.toLocaleString()+`₪`)
+    .addField(`Tank Tactics`,'_ _')
+    .addField(`Games Played`,user.gameplayed,true)
+    .addField('Wins',user.wins,true)
+    .addField('Kills',user.kills,true)
+    .addField('Deaths',user.deaths,true)
+    .addField('Moves',user.moves,true)
+    .addField('Donations',user.dontations,true)
+    .addField('Heals',user.heals,true)
+
+
+    message.reply({embeds:[embed]});
+}   
 };
