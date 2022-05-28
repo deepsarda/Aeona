7443c86395df232123edd6e7ae33da9f1bd7195f
multiii <72256091+multiii@users.noreply.github.com>
2022-05-28 20:40:14 +0530

    Add invite.js

---
+++ commands/utility/invite.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "invite",
+    description: "Invite aeona to your servers",
+    usage: "+invite",
+    execute: async (message, args, bot, prefix) => {
+        await message.channel.send({
+            title: "Invite aeona",
+            description: "[Invite me to your servers by clicking here](https://aeona.xyz)!"
+        })
+    }
+};
