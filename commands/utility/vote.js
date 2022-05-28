8d8afff38c5ff16c3f6263982c702fb71171f45c
multiii <72256091+multiii@users.noreply.github.com>
2022-05-28 14:16:33 +0530

    Add vote command

---
+++ commands/utility/vote.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "vote",
+    description: "Vote for aeona",
+    usage: "+vote",
+    execute: async (message, args, bot, prefix) => {
+        await message.channel.send({
+            title: "Vote for aeona",
+            description: "Vote for aeona on [top.gg](https://top.gg/bot/931226824753700934/vote)\nOnce you're done, check out our site [here](https://aeona.xyz)!"
+        })
+    }
+};
