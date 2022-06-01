5e2c40c1e52ef7dcf2752ad27de49a9c7aea0acb
deepsarda <92147339+deepsarda@users.noreply.github.com>
2022-06-01 17:53:53 +0530

    bug fixes

--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -763,9 +763,7 @@ async function execute(message, prefix, i, chatbot) {
         port: null,
         path: encodeURI(
           "/?" +
-            `text=${message.content}&userId=${message.author.id}&key=${
-              process.env.apiKey
-            }${context ? `&context=${context}` : ""}${
+            `text=${message.content}&userId=${message.author.id}${context ? `&context=${context}` : ""}${
               context1 ? `&context1=${context1}` : ""
             } ${context2 ? `&context2=${context2}` : ""} ${
               context3 ? `&context3=${context3}` : ""
