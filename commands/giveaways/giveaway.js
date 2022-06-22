acfe04adc42fd39ca8bb66d51ee2b8c481e76b2f
deepparag <deep.p.sarda@gmail.com>
2022-06-22 17:08:55 +0530

    tweak giveaway command

--- commands/giveaways/giveaway.js
+++ commands/giveaways/giveaway.js
@@ -12,11 +12,23 @@ module.exports = {
     const duration = args[0];
     const winnerCount = Number(args[1]);
     const prize = args.slice(2).join(" ");
+    if(!duration)
+      return message.channel.send(`${message.author}, you need to specify a duration!`);
     
+    if(!winnerCount)
+      return message.channel.send(`${message.author}, you need to specify a winner count!`);
+
+    if(!prize)
+      return message.channel.send(`${message.author}, you need to specify a prize!`);
+    
+
+  
     await bot.giveawaysManager.start(message.channel, {
       duration: ms(duration),
       winnerCount,
-      prize
+      prize,
+      hostedBy: message.author,
+      
     });
   },
 };
