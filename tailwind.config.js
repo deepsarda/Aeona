4881c8c7d564415f2afa526b88e5259622dbe5ea
Deep Sarda <92147339+deepsarda@users.noreply.github.com>
2022-07-10 10:54:26 +0000

    new website design!

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
           "secondary-content": "#ffffff",
         },
       },
+      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
     ],
   },
   theme: {
