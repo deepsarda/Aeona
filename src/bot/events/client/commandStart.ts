d1bd91169ba95afe1172af3e25f54df77c632c2c
deepparag <deep.p.sarda@gmail.com>
2022-11-07 16:47:09 +0530

    Format

--- src/bot/events/client/commandStart.ts
+++ src/bot/events/client/commandStart.ts
@@ -4,7 +4,7 @@ import { Interaction, Message } from 'discordeno/transformers';
 import { Influx } from '../../../analytics.js';
 export default async (bot: AmethystBot, command: CommandClass, data: Interaction | Message) => {
 	console.log(command.name);
-	console.log(Influx)
+	console.log(Influx);
 	Influx?.writePoint(new Point('commandStart').tag('commandName', command.name));
 	Influx?.writePoint(new Point('commands').tag('action', 'addition').tag('command', command.name).intField('value', 1));
 };
