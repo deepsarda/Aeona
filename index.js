106e42af38bd366a71f95e06efa0e389025c78e1
deepparag <deep.p.sarda@gmail.com>
2022-11-07 16:32:30 +0530

    Format Files

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { spawn } from 'child_process';
 const ls = spawn('npm', ['run', 'devb']);
 
 ls.stdout.on('data', (data) => {
-  console.log(data);
+	console.log(data);
 });
 
 ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+	console.error(`stderr: ${data}`);
 });
 
 ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+	console.log(`child process exited with code ${code}`);
 });
