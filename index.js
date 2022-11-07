049dbb0427e86244d17642c355de9bae49915f2d
deepparag <deep.p.sarda@gmail.com>
2022-11-07 16:20:15 +0530

    Add start file

---
+++ index.js
@@ -0,0 +1,14 @@
+import { spawn } from 'child_process';
+const ls = spawn('npm', ['run', 'devb']);
+
+ls.stdout.on('data', (data) => {
+  console.log(data);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
