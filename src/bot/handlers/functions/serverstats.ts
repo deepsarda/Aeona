86a59c784b71a04624be4b64ebd7257ea1660b65
deepparag <deep.p.sarda@gmail.com>
2022-11-13 13:32:33 +0530

    Fix serverstats

--- src/bot/handlers/functions/serverstats.ts
+++ src/bot/handlers/functions/serverstats.ts
@@ -18,61 +18,61 @@ export default async (client: AmethystBot) => {
 	};
 
 	client.on('guildMemberAdd', async (client: AmethystBot, member: Member) => {
-		client.emit('updateMembers', client, await client.cache.guilds.get(member.guildId));
-		client.emit('updateBots', client, await client.cache.guilds.get(member.guildId));
+		client.emit('updateMembers', client, await client.helpers.getGuild(member.guildId));
+		client.emit('updateBots', client, await client.helpers.getGuild(member.guildId));
 	});
 	client.on('guildMemberRemove', async (client: AmethystBot, member: Member) => {
-		client.emit('updateMembers', client, await client.cache.guilds.get(member.guildId));
-		client.emit('updateBots', client, await client.cache.guilds.get(member.guildId));
+		client.emit('updateMembers', client, await client.helpers.getGuild(member.guildId));
+		client.emit('updateBots', client, await client.helpers.getGuild(member.guildId));
 	});
 
 	client.on('channelCreate', async (client: AmethystBot, channel: Channel) => {
 		if (!channel.guildId) return;
-		client.emit('updateChannels', channel, client, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateNewsChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateStageChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateTextChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateVoiceChannels', client, channel, await client.cache.guilds.get(channel.guildId));
+		client.emit('updateChannels', channel, client, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateNewsChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateStageChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateTextChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateVoiceChannels', client, channel, await client.helpers.getGuild(channel.guildId));
 	});
 	client.on('channelDelete', async (client: AmethystBot, channel: Channel) => {
 		if (!channel.guildId) return;
-		client.emit('updateChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateNewsChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateStageChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateTextChannels', client, channel, await client.cache.guilds.get(channel.guildId));
-		client.emit('updateVoiceChannels', client, channel, await client.cache.guilds.get(channel.guildId));
+		client.emit('updateChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateNewsChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateStageChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateTextChannels', client, channel, await client.helpers.getGuild(channel.guildId));
+		client.emit('updateVoiceChannels', client, channel, await client.helpers.getGuild(channel.guildId));
 	});
 
 	client.on('roleCreate', async (client: AmethystBot, role: Role) =>
-		client.emit('updateRoles', client, await client.cache.guilds.get(role.guildId)),
+		client.emit('updateRoles', client, await client.helpers.getGuild(role.guildId)),
 	);
 	client.on('roleDelete', async (client: AmethystBot, role: Role) =>
-		client.emit('updateRoles', client, await client.cache.guilds.get(role.guildId)),
+		client.emit('updateRoles', client, await client.helpers.getGuild(role.guildId)),
 	);
 
 	client.on('guildMemberBoost', async (booster: Member) =>
-		client.emit('updateBoosts', client, await client.cache.guilds.get(booster.guildId)),
+		client.emit('updateBoosts', client, await client.helpers.getGuild(booster.guildId)),
 	);
 	client.on('guildMemberUnboost', async (booster: Member) =>
-		client.emit('updateBoosts', client, await client.cache.guilds.get(booster.guildId)),
+		client.emit('updateBoosts', client, await client.helpers.getGuild(booster.guildId)),
 	);
 
 	client.on('guildBoostLevelUp', async (tier: Guild) =>
-		client.emit('updateTier', client, await client.cache.guilds.get(tier.id)),
+		client.emit('updateTier', client, await client.helpers.getGuild(tier.id)),
 	);
 	client.on('guildBoostLevelDown', async (tier: Guild) =>
-		client.emit('updateTier', client, await client.cache.guilds.get(tier.id)),
+		client.emit('updateTier', client, await client.helpers.getGuild(tier.id)),
 	);
 
 	client.on('emojiCreate', async (emoji) => {
-		client.emit('updateEmojis', client, emoji, await client.cache.guilds.get(emoji.guildId));
-		client.emit('updateAEmojis', client, emoji, await client.cache.guilds.get(emoji.guildId));
-		client.emit('updateSEmojis', client, emoji, await client.cache.guilds.get(emoji.guildId));
+		client.emit('updateEmojis', client, emoji, await client.helpers.getGuild(emoji.guildId));
+		client.emit('updateAEmojis', client, emoji, await client.helpers.getGuild(emoji.guildId));
+		client.emit('updateSEmojis', client, emoji, await client.helpers.getGuild(emoji.guildId));
 	});
 	client.on('emojiDelete', async (emoji) => {
-		client.emit('updateEmojis', client, emoji, await client.cache.guilds.get(emoji.guildId));
-		client.emit('updateAEmojis', client, emoji, await client.cache.guilds.get(emoji.guildId));
-		client.emit('updateSEmojis', client, emoji, await client.cache.guilds.get(emoji.guildId));
+		client.emit('updateEmojis', client, emoji, await client.helpers.getGuild(emoji.guildId));
+		client.emit('updateAEmojis', client, emoji, await client.helpers.getGuild(emoji.guildId));
+		client.emit('updateSEmojis', client, emoji, await client.helpers.getGuild(emoji.guildId));
 	});
 
 	client.on('ready', async (client) => client.emit('updateClock', client));
