717a99ecdb8426848fdb0cef89b2f520620b385a
deepparag <deep.p.sarda@gmail.com>
2022-11-14 16:24:10 +0530

    Update Dependencies

--- src/bot/events/client/commandError.ts
+++ src/bot/events/client/commandError.ts
@@ -82,4 +82,6 @@ export default async (
 		return await bot.helpers.sendMessage(data.message ? data.message.channelId : data.data?.channelId, {
 			content: 'Oh no! This is only meant for my owner.',
 		});
+
+    console.log(data.error)
 };
