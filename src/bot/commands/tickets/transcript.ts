c1f796f3bfc8296a1b5f019c755d6d30da25c98a
deepparag <deep.p.sarda@gmail.com>
2022-12-12 16:11:33 +0530

    improvements

--- src/bot/commands/tickets/transcript.ts
+++ src/bot/commands/tickets/transcript.ts
@@ -1,5 +1,3 @@
-import ticketSchema from '../../database/models/tickets.js';
-import ticketChannels from '../../database/models/ticketChannels.js';
 
 import { AmethystBot, Context } from '@thereallonewolf/amethystframework';
 export default {
@@ -14,62 +12,24 @@ export default {
 
 		const type = 'reply';
 
-		ticketChannels.findOne({ Guild: ctx.guildId, channelID: ctx.channel.id }, async (err, ticketData) => {
-			if (ticketData) {
-				ticketSchema.findOne({ Guild: ctx.guildId }, async (err, data) => {
-					if (data) {
-						const ticketCategory = await client.helpers.getChannel(data.Category);
-
-						if (ticketCategory == undefined) {
-							return client.extras.errNormal(
-								{
-									error: 'Do the setup!',
-									type: type,
-								},
-								ctx,
-							);
-						}
-
-						if (ctx.channel.parentId == ticketCategory.id) {
-							return client.extras
-								.simpleEmbed(
-									{
-										desc: `${client.extras.emotes.animated.loading} Transcript saving...`,
-										type: type,
-									},
-									ctx,
-								)
-								.then(async (editMsg) => {
-									client.extras.transcript(client, ctx.channel).then(() => {
-										return client.extras.simpleEmbed(
-											{
-												desc: `Transcript saved`,
-												type: 'editreply',
-											},
-											ctx,
-										);
-									});
-								});
-						} else {
-							client.extras.errNormal(
-								{
-									error: 'This is not a ticket!',
-									type: type,
-								},
-								ctx,
-							);
-						}
-					} else {
-						return client.extras.errNormal(
-							{
-								error: 'Do the setup!',
-								type: type,
-							},
-							ctx,
-						);
-					}
+		return client.extras
+			.simpleEmbed(
+				{
+					desc: `${client.extras.emotes.animated.loading} Transcript saving...`,
+					type: type,
+				},
+				ctx,
+			)
+			.then(async (editMsg) => {
+				client.extras.transcript(client, ctx.channel).then(() => {
+					return client.extras.simpleEmbed(
+						{
+							desc: `Transcript saved`,
+							type: 'editreply',
+						},
+						ctx,
+					);
 				});
-			}
-		});
+			});
 	},
 };
