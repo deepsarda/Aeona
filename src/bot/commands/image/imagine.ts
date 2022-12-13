cebe7fe14d1769068d64be72a2f2d6515f1af02b
deepparag <deep.p.sarda@gmail.com>
2022-12-13 17:25:36 +0530

    format code

--- src/bot/commands/image/imagine.ts
+++ src/bot/commands/image/imagine.ts
@@ -215,14 +215,19 @@ export default {
 					],
 				});
 				const component = new Components();
-				component.addSelectComponent('Share your image!', 'share-imagine', [
-					{
-						label: 'Official Server',
-						value: 'share-discord',
-						description:
-							'Share your image on the official discord server. Note: Your discord username and id will be shared.',
-					},
-				],"Share this image.");
+				component.addSelectComponent(
+					'Share your image!',
+					'share-imagine',
+					[
+						{
+							label: 'Official Server',
+							value: 'share-discord',
+							description:
+								'Share your image on the official discord server. Note: Your discord username and id will be shared.',
+						},
+					],
+					'Share this image.',
+				);
 				ctx.reply({
 					content: '**Prompt:** ' + prompt + '\n **Mode:** ' + c.data.values[0],
 					file: [
