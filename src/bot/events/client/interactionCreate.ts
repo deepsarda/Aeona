74a2b2089a9c3f30bca213b62bee169789a69602
deepparag <deep.p.sarda@gmail.com>
2022-12-13 18:42:24 +0530

    fixes

--- src/bot/events/client/interactionCreate.ts
+++ src/bot/events/client/interactionCreate.ts
@@ -1,15 +1,7 @@
 import { AmethystBot, AmethystEmbed, createContext, createOptionResults } from '@thereallonewolf/amethystframework';
 import { Message } from 'discordeno';
-import { Channel, Embed, Interaction } from 'discordeno/transformers';
-import {
-	AllowedMentions,
-	BigString,
-	DiscordChannel,
-	FileContent,
-	InteractionTypes,
-	MessageComponents,
-	WithReason,
-} from 'discordeno/types';
+import { Interaction } from 'discordeno/transformers';
+import { InteractionTypes } from 'discordeno/types';
 
 import Captcha from '@haileybot/captcha-generator';
 import reactionSchema from '../../database/models/reactionRoles.js';
@@ -205,7 +197,7 @@ export default async (client: AmethystBot, interaction: Interaction) => {
 				interaction.data?.values[0] == 'share-discord' &&
 				interaction.data?.customId.split('_')[1] == interaction.user.id + ''
 			) {
-				const channel = await createForumThread(client, '1045332279943233667', {
+				const channel = await client.helpers.createForumThread('1045332279943233667', {
 					name:
 						interaction.message.content.split('\n')[0].split(':')[1] +
 						' by ' +
@@ -335,60 +327,3 @@ export default async (client: AmethystBot, interaction: Interaction) => {
 		notice.execute(client, ctx);
 	}
 };
-type Field = {
-	name: string;
-	value: string;
-	inline?: boolean;
-};
-export async function createForumThread(
-	bot: AmethystBot,
-	channelId: BigString,
-	options: CreateForumPostWithMessage,
-): Promise<Channel> {
-	const result = await bot.rest.runMethod<DiscordChannel>(
-		bot.rest,
-		'POST',
-		bot.constants.routes.FORUM_START(channelId),
-		{
-			name: options.name,
-			auto_archive_duration: options.autoArchiveDuration,
-			rate_limit_per_user: options.rateLimitPerUser,
-			reason: options.reason,
-			message: {
-				content: options.content,
-				embeds: options.embeds?.map((embed) => bot.transformers.reverse.embed(bot, embed)),
-				allowed_mentions: options.allowedMentions
-					? {
-							parse: options.allowedMentions?.parse,
-							roles: options.allowedMentions?.roles?.map((id) => id.toString()),
-							users: options.allowedMentions?.users?.map((id) => id.toString()),
-							replied_user: options.allowedMentions?.repliedUser,
-					  }
-					: undefined,
-				file: options.file,
-				components: options.components?.map((component) => bot.transformers.reverse.component(bot, component)),
-			},
-		},
-	);
-
-	return bot.transformers.channel(bot, { channel: result, guildId: bot.transformers.snowflake(result.guild_id!) });
-}
-
-export interface CreateForumPostWithMessage extends WithReason {
-	/** 1-100 character thread name */
-	name: string;
-	/** Duration in minutes to automatically archive the thread after recent activity */
-	autoArchiveDuration: 60 | 1440 | 4320 | 10080;
-	/** Amount of seconds a user has to wait before sending another message (0-21600) */
-	rateLimitPerUser?: number | null;
-	/** The message contents (up to 2000 characters) */
-	content?: string;
-	/** Embedded `rich` content (up to 6000 characters) */
-	embeds?: Embed[];
-	/** Allowed mentions for the message */
-	allowedMentions?: AllowedMentions;
-	/** The contents of the file being sent */
-	file?: FileContent | FileContent[];
-	/** The components you would like to have sent in this message */
-	components?: MessageComponents;
-}
