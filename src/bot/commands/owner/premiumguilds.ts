9c4756f126ae9c7a8812f930b1e8c6f5134a1286
deepparag <deep.p.sarda@gmail.com>
2022-12-27 16:10:20 +0530

    Improvements

---
+++ src/bot/commands/owner/premiumguilds.ts
@@ -0,0 +1,33 @@
+
+
+import { CommandOptions, Context } from '@thereallonewolf/amethystframework';
+import { AeonaBot } from '../../extras/index.js';
+import functions from '../../database/models/functions.js';
+export default {
+	name: 'premiumguilds',
+	description: 'See the number of premium guilds',
+	commandType: ['application', 'message'],
+	category: 'owner',
+	args: [
+	],
+	ownerOnly: true,
+	async execute(client: AeonaBot, ctx: Context) {
+		if (!ctx.guild || !ctx.user || !ctx.channel) return console.log(ctx.guild + ' ' + ctx.channel + ' ' + ctx.user);
+		const premiumGuilds: string[] = [];
+		const premium = await functions.find({ isPremium: "true" });
+		for (let i = 0; i < premium.length; i++) {
+			if (premium[i].isPremium === "true") {
+				const guild = await client.cache.guilds.get(BigInt(premium[i].Guild!));
+				if (guild)
+					premiumGuilds.push(`${guild.name} ${guild.id}`);
+			}
+		}
+		client.extras.succNormal(
+			{
+				text: `There are a total of ${premiumGuilds.length} premium servers. \n\n` + premiumGuilds.join("\n\n"),
+				type: 'reply',
+			},
+			ctx,
+		);
+	},
+} as CommandOptions;
--- src/bot/events/client/guildCreate.ts
+++ src/bot/events/client/guildCreate.ts
@@ -1,6 +1,7 @@
 import { Guild } from 'discordeno/transformers';
 import Functions from '../../database/models/functions.js';
 import { AeonaBot } from '../../extras/index.js';
+import { AmethystEmbed } from '@thereallonewolf/amethystframework';
 
 export default async (client: AeonaBot, guild: Guild) => {
 	if (guild == undefined) return;
@@ -9,18 +10,26 @@ export default async (client: AeonaBot, guild: Guild) => {
 		Guild: guild.id + '',
 		Prefix: process.env.PREFIX,
 	}).save();
+	if (guild.systemChannelId) {
+		const channel = guild.channels.get(guild.systemChannelId);
+		if (channel) {
 
-	/*
-	const embed = new AmethystEmbed()
-		.setTitle('Added to a new server!')
-		.addField('Total servers:', `${client.cache.guilds.memory.size}`, true)
-		.addField('Server name', `${guild.name}`, true)
-		.addField('Server ID', `${guild.id}`, true)
-		.addField('Server members', `${guild.memberCount}`, true)
-		.addField('Server owner', `<@${guild.ownerId}> (${guild.ownerId})`, true)
-		.setColor(client.extras.config.colors.normal);
-	client.extras.webhook({
-		embeds: [embed],
-	});
-	*/
+			client.helpers.followAnnouncementChannel("1050412811353858128", channel.id)
+		}
+	}
+	if (Date.now() > client.extras.startTime + 10 * 60 * 1000) {
+
+		const embed = new AmethystEmbed()
+			.setTitle('Added to a new server!')
+			.addField('Total servers:', `${client.cache.guilds.memory.size}`, true)
+			.addField('Server name', `${guild.name}`, true)
+			.addField('Server ID', `${guild.id}`, true)
+			.addField('Server members', `${guild.memberCount}`, true)
+			.addField('Server owner', `<@${guild.ownerId}> (${guild.ownerId})`, true)
+			.setColor(client.extras.config.colors.normal);
+		client.extras.webhook({
+			embeds: [embed],
+		});
+	}
+
 };
