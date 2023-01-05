31d96f7853fc03eb54e37cfd2ddf91a471c96d60
deepparag <deep.p.sarda@gmail.com>
2023-01-05 22:30:33 +0530

    format code

--- src/bot/commands/image/quote.ts
+++ src/bot/commands/image/quote.ts
@@ -4,23 +4,23 @@ import fetch from 'node-fetch';
 import { AeonaBot } from '../../extras/index.js';
 
 export default {
-    name: 'quote',
-    description: 'Get a AI generated quote',
-    commandType: ['application', 'message'],
-    category: 'image',
-    args: [
+	name: 'quote',
+	description: 'Get a AI generated quote',
+	commandType: ['application', 'message'],
+	category: 'image',
+	args: [],
 
-    ],
+	async execute(client: AeonaBot, ctx: Context) {
+		if (!ctx.guild || !ctx.user || !ctx.channel) return console.log('Hmm');
 
-    async execute(client: AeonaBot, ctx: Context) {
+		const url = await (await fetch('https://inspirobot.me/api?generate=true')).text();
 
-        if (!ctx.guild || !ctx.user || !ctx.channel) return console.log('Hmm');
-
-        const url = await (await fetch("https://inspirobot.me/api?generate=true")).text();
-
-        client.extras.embed({
-            title: 'Here\'s a quote for you',
-            image: url
-        }, ctx)
-    }
+		client.extras.embed(
+			{
+				title: "Here's a quote for you",
+				image: url,
+			},
+			ctx,
+		);
+	},
 } as CommandOptions;
