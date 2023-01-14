5f6d089dd5c25ad3406148c670e75b7854f021e1
deepparag <deep.p.sarda@gmail.com>
2023-01-14 11:18:58 +0530

    Revert to eris rest

---
+++ src/rest/errors/DiscordRESTError.ts
@@ -0,0 +1,74 @@
+export class DiscordRESTError extends Error {
+	response: any;
+	res: any;
+	code: number | undefined;
+
+	constructor(req: any, res: any, response: any, stack: any) {
+		super();
+
+		Object.defineProperty(this, 'req', {
+			enumerable: false,
+			value: req,
+		});
+		Object.defineProperty(this, 'res', {
+			enumerable: false,
+			value: res,
+		});
+		Object.defineProperty(this, 'response', {
+			enumerable: false,
+			value: response,
+		});
+
+		Object.defineProperty(this, 'code', {
+			enumerable: false,
+			value: +response.code || -1,
+		});
+		let message = response.message || 'Unknown error';
+		if (response.errors) {
+			message += '\n  ' + this.flattenErrors(response.errors).join('\n  ');
+		} else {
+			const errors = this.flattenErrors(response);
+			if (errors.length > 0) {
+				message += '\n  ' + errors.join('\n  ');
+			}
+		}
+		Object.defineProperty(this, 'message', {
+			enumerable: false,
+			value: message,
+		});
+
+		if (stack) {
+			this.stack = this.name + ': ' + this.message + '\n' + stack;
+		} else {
+			Error.captureStackTrace(this, DiscordRESTError);
+		}
+	}
+
+	get headers() {
+		return this.response.headers;
+	}
+
+	override get name() {
+		return `${this.constructor.name} [${this.code}]`;
+	}
+
+	flattenErrors(errors: any, keyPrefix = '') {
+		let messages: string[] = [];
+		for (const fieldName in errors) {
+			// eslint-disable-next-line no-prototype-builtins
+			if (!errors.hasOwnProperty(fieldName) || fieldName === 'message' || fieldName === 'code') {
+				continue;
+			}
+			if (errors[fieldName]._errors) {
+				messages = messages.concat(
+					errors[fieldName]._errors.map((obj: any) => `${keyPrefix + fieldName}: ${obj.message}`),
+				);
+			} else if (Array.isArray(errors[fieldName])) {
+				messages = messages.concat(errors[fieldName].map((str: any) => `${keyPrefix + fieldName}: ${str}`));
+			} else if (typeof errors[fieldName] === 'object') {
+				messages = messages.concat(this.flattenErrors(errors[fieldName], keyPrefix + fieldName + '.'));
+			}
+		}
+		return messages;
+	}
+}
---
+++ src/rest/util/MultipartData.ts
@@ -0,0 +1,65 @@
+export class MultipartData {
+	boundary: string;
+	bufs: any[];
+
+	constructor() {
+		this.boundary = '----------------Eris';
+		this.bufs = [];
+	}
+
+	attach(fieldName: string, data: any, filename: string) {
+		if (data === undefined) {
+			return;
+		}
+		let str = '\r\n--' + this.boundary + '\r\nContent-Disposition: form-data; name="' + fieldName + '"';
+		let contentType;
+		if (filename) {
+			str += '; filename="' + filename + '"';
+			const extension = filename.match(/\.(png|apng|gif|jpg|jpeg|webp|svg|json)$/i);
+			if (extension) {
+				let ext = extension[1].toLowerCase();
+				switch (ext) {
+					case 'png':
+					case 'apng':
+					case 'gif':
+					case 'jpg':
+					case 'jpeg':
+					case 'webp':
+					case 'svg': {
+						if (ext === 'svg') {
+							ext = 'svg+xml';
+						}
+						contentType = 'image/';
+						break;
+					}
+					case 'json': {
+						contentType = 'application/';
+						break;
+					}
+				}
+				contentType += ext;
+			}
+		}
+
+		if (contentType) {
+			str += `\r\nContent-Type: ${contentType}`;
+		} else if (ArrayBuffer.isView(data)) {
+			str += '\r\nContent-Type: application/octet-stream';
+			if (!(data instanceof Uint8Array)) {
+				data = new Uint8Array(data.buffer, data.byteOffset, data.byteLength);
+			}
+		} else if (typeof data === 'object') {
+			str += '\r\nContent-Type: application/json';
+			data = Buffer.from(JSON.stringify(data));
+		} else {
+			data = Buffer.from('' + data);
+		}
+		this.bufs.push(Buffer.from(str + '\r\n\r\n'));
+		this.bufs.push(data);
+	}
+
+	finish() {
+		this.bufs.push(Buffer.from('\r\n--' + this.boundary + '--'));
+		return this.bufs;
+	}
+}
