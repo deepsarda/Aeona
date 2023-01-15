d00d1f77e3c664e79ceece0bfb54083e41bbf75e
deepparag <deep.p.sarda@gmail.com>
2023-01-15 22:00:09 +0530

    Fix b64 to blob

--- packages/rest/src/requests/runMethod.ts
+++ packages/rest/src/requests/runMethod.ts
@@ -18,14 +18,7 @@ export default async (data: RunMethod, rest: RestManager): Promise<unknown> => {
   }
 
   if (data.body && (data.body as any)?.file != undefined) {
-    console.log((data.body as any).file[0].blob);
-
-    (data.body as any).file[0].blob = new Blob(
-      [Buffer.from((data.body as any).file[0].blob, 'base64')],
-      {
-        encoding: 'base64',
-      },
-    );
+    (data.body as any).file[0].blob = b64toBlob((data.body as any).file[0].blob);
     console.log(data.body);
   }
 
@@ -47,4 +40,23 @@ export default async (data: RunMethod, rest: RestManager): Promise<unknown> => {
     });
 
   return result;
+};
+const b64toBlob = (b64Data: string, contentType = '', sliceSize = 512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays: Uint8Array[] = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType });
+  return blob;
 };
