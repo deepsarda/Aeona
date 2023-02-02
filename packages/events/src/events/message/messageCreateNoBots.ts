870748fbdabb5464dbd2910691cb99be0c061e9e
deepparag <deep.p.sarda@gmail.com>
2023-02-02 18:53:17 +0530

    Bug Fixes

--- packages/events/src/events/message/messageCreateNoBots.ts
+++ packages/events/src/events/message/messageCreateNoBots.ts
@@ -37,100 +37,125 @@ export default async (client: AeonaBot, message: Message) => {
       {
         title: `Hi there!`,
         desc: `My prefix is \`${guild.Prefix}\`.
-Let me help you get your server going.	
+Let me help you get your server going.
 
 **Want to setup chatbot?**
 Use \`${guild.Prefix}setup chatbot <channel>\` or
 \`${guild.Prefix}autosetup chatbot\` to have me make a channel for you.
-			
+
 **Want to setup bump reminder?**
 Well then run \`${guild.Prefix}bumpreminder setup <channel> <role>\`
-			
+
 **Want to generate some art?**
 Use \`${guild.Prefix}imagine <prompt>\`
-				
+
 Use the  \`${guild.Prefix}help\` to see all my commands.`,
       },
       message,
     );
   }
   // Levels
-  Functions.findOne({ Guild: message.guildId }, async (err: any, data: { Levels: boolean }) => {
-    if (data) {
-      if (data.Levels == true) {
-        const randomXP = Math.floor(Math.random() * 9) + 1;
-        const hasLeveledUp = await client.extras.addXP(
-          message.authorId,
-          message.guildId!,
-          randomXP,
-        );
+  Functions.findOne(
+    { Guild: message.guildId },
+    async (err: any, data: { Levels: boolean }) => {
+      if (data) {
+        if (data.Levels == true) {
+          const randomXP = Math.floor(Math.random() * 9) + 1;
+          const hasLeveledUp = await client.extras.addXP(
+            message.authorId,
+            message.guildId!,
+            randomXP,
+          );
 
-        if (hasLeveledUp) {
-          const user = await client.extras.fetchLevels(message.authorId, message.guildId!);
-          if (!user) return;
-          const levelData = await levelLogs.findOne({
-            Guild: message.guildId,
-          });
-          const messageData = await messageSchema.findOne({
-            Guild: message.guildId,
-          });
-          const u = await client.helpers.getUser(message.authorId);
-          if (messageData) {
-            let levelMessage = messageData.Message;
-            if (!levelMessage) return;
-            levelMessage = levelMessage.replace(`{user:username}`, u.username);
-            levelMessage = levelMessage.replace(`{user:discriminator}`, u.discriminator);
-            levelMessage = levelMessage.replace(`{user:tag}`, u.username + '#' + u.discriminator);
-            levelMessage = levelMessage.replace(`{user:mention}`, '<@' + u.id + '>');
+          if (hasLeveledUp) {
+            const user = await client.extras.fetchLevels(
+              message.authorId,
+              message.guildId!,
+            );
+            if (!user) return;
+            const levelData = await levelLogs.findOne({
+              Guild: message.guildId,
+            });
+            const messageData = await messageSchema.findOne({
+              Guild: message.guildId,
+            });
+            const u = await client.helpers.getUser(message.authorId);
+            if (messageData) {
+              let levelMessage = messageData.Message;
+              if (!levelMessage) return;
+              levelMessage = levelMessage.replace(
+                `{user:username}`,
+                u.username,
+              );
+              levelMessage = levelMessage.replace(
+                `{user:discriminator}`,
+                u.discriminator,
+              );
+              levelMessage = levelMessage.replace(
+                `{user:tag}`,
+                u.username + '#' + u.discriminator,
+              );
+              levelMessage = levelMessage.replace(
+                `{user:mention}`,
+                '<@' + u.id + '>',
+              );
 
-            levelMessage = levelMessage.replace(`{user:level}`, user.level + '');
-            levelMessage = levelMessage.replace(`{user:xp}`, user.xp + '');
+              levelMessage = levelMessage.replace(
+                `{user:level}`,
+                user.level + '',
+              );
+              levelMessage = levelMessage.replace(`{user:xp}`, user.xp + '');
 
-            try {
-              if (levelData) {
-                await client.helpers.sendMessage(levelData.Channel!, {
-                  content: levelMessage,
-                });
-              } else {
+              try {
+                if (levelData) {
+                  await client.helpers.sendMessage(levelData.Channel!, {
+                    content: levelMessage,
+                  });
+                } else {
+                  await client.helpers.sendMessage(message.channelId, {
+                    content: levelMessage,
+                  });
+                }
+              } catch {
                 await client.helpers.sendMessage(message.channelId, {
                   content: levelMessage,
                 });
               }
-            } catch {
-              await client.helpers.sendMessage(message.channelId, {
-                content: levelMessage,
-              });
-            }
-          } else {
-            try {
-              if (levelData) {
-                client.helpers.sendMessage(levelData.Channel!, {
-                  content: `**GG** <@!${message.authorId}>, you are now level **${user.level}**`,
-                });
-              } else {
+            } else {
+              try {
+                if (levelData) {
+                  client.helpers.sendMessage(levelData.Channel!, {
+                    content: `**GG** <@!${message.authorId}>, you are now level **${user.level}**`,
+                  });
+                } else {
+                  client.helpers.sendMessage(message.channelId, {
+                    content: `**GG** <@!${message.authorId}>, you are now level **${user.level}**`,
+                  });
+                }
+              } catch {
                 client.helpers.sendMessage(message.channelId, {
                   content: `**GG** <@!${message.authorId}>, you are now level **${user.level}**`,
                 });
               }
-            } catch {
-              client.helpers.sendMessage(message.channelId, {
-                content: `**GG** <@!${message.authorId}>, you are now level **${user.level}**`,
-              });
             }
-          }
 
-          levelRewards.findOne(
-            { Guild: message.guildId, Level: user.level },
-            async (err: any, data: { Role: BigString }) => {
-              if (data) {
-                await client.helpers.addRole(message.guildId!, message.authorId, data.Role);
-              }
-            },
-          );
+            levelRewards.findOne(
+              { Guild: message.guildId, Level: user.level },
+              async (err: any, data: { Role: BigString }) => {
+                if (data) {
+                  await client.helpers.addRole(
+                    message.guildId!,
+                    message.authorId,
+                    data.Role,
+                  );
+                }
+              },
+            );
+          }
         }
       }
-    }
-  });
+    },
+  );
 
   // Message tracker system
   messagesSchema.findOne(
@@ -145,7 +170,11 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
           async (err: any, data: { Role: BigString }) => {
             if (data) {
               try {
-                await client.helpers.addRole(message.guildId!, message.authorId, data.Role);
+                await client.helpers.addRole(
+                  message.guildId!,
+                  message.authorId,
+                  data.Role,
+                );
               } catch {
                 //prevent lint error
               }
@@ -163,46 +192,55 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
   );
 
   // AFK system
-  afk.findOne({ Guild: message.guildId, User: message.authorId }, async (err: any, data: any) => {
-    if (data) {
-      await afk.deleteOne({
-        Guild: message.guildId,
-        User: message.authorId,
-      });
-
-      client.extras.simpleMessageEmbed(
-        {
-          desc: `<@${message.authorId}> is no longer afk!`,
-        },
-        message,
-      );
-
-      if (message.member?.nick?.startsWith(`[AFK] `)) {
-        const name = message.member?.nick?.replace(`[AFK] `, ``);
-        client.helpers.editMember(message.guildId!, message.authorId, {
-          nick: name,
+  afk.findOne(
+    { Guild: message.guildId, User: message.authorId },
+    async (err: any, data: any) => {
+      if (data) {
+        await afk.deleteOne({
+          Guild: message.guildId,
+          User: message.authorId,
         });
+
+        client.extras.simpleMessageEmbed(
+          {
+            desc: `<@${message.authorId}> is no longer afk!`,
+          },
+          message,
+        );
+
+        if (message.member?.nick?.startsWith(`[AFK] `)) {
+          const name = message.member?.nick?.replace(`[AFK] `, ``);
+          client.helpers.editMember(message.guildId!, message.authorId, {
+            nick: name,
+          });
+        }
       }
-    }
-  });
+    },
+  );
 
   message.mentionedUserIds.forEach(async (u) => {
-    if (!message.content.includes('@here') && !message.content.includes('@everyone')) {
-      afk.findOne({ Guild: message.guildId, User: u }, async (err: any, data: { Message: any }) => {
-        if (data) {
-          client.extras.simpleMessageEmbed(
-            { desc: `<@${u}> is currently afk! **Reason:** ${data.Message}` },
-            message,
-          );
-        }
-      });
+    if (
+      !message.content.includes('@here') &&
+      !message.content.includes('@everyone')
+    ) {
+      afk.findOne(
+        { Guild: message.guildId, User: u },
+        async (err: any, data: { Message: any }) => {
+          if (data) {
+            client.extras.simpleMessageEmbed(
+              { desc: `<@${u}> is currently afk! **Reason:** ${data.Message}` },
+              message,
+            );
+          }
+        },
+      );
     }
   });
 
   // Chat bot
   chatBotSchema.findOne({ Guild: message.guildId }, async (err, data) => {
     if (!data) return;
-
+    if (message.mentionedUserIds.includes(client.user.id)) return;
     if (message.channelId != data.Channel) return;
     const msgs: Message[] = Array.from(
       (
@@ -234,9 +272,11 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
       message.authorId +
       '&key=' +
       process.env.apiKey +
-      `${context ? `&context=${context}` : ''}${context1 ? `&context1=${context1}` : ''} ${
-        context2 ? `&context2=${context2}` : ''
-      } ${context3 ? `&context3=${context3}` : ''} ${context4 ? `&context4=${context4}` : ''} ${
+      `${context ? `&context=${context}` : ''}${
+        context1 ? `&context1=${context1}` : ''
+      } ${context2 ? `&context2=${context2}` : ''} ${
+        context3 ? `&context3=${context3}` : ''
+      } ${context4 ? `&context4=${context4}` : ''} ${
         context5 ? `&context5=${context5}` : ''
       }`;
 
@@ -254,7 +294,10 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
             .intField('value', 1),
         );
 
-        let s = ['\n discord.gg/qURxRRHPwa', '\n Generate beautiful images using /imagine \n '];
+        let s = [
+          '\n discord.gg/qURxRRHPwa',
+          '\n Generate beautiful images using /imagine \n ',
+        ];
         let guild = await Functions.findOne({
           Guild: message.guildId,
         });
@@ -263,7 +306,11 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
         console.log(`BOT`.blue.bold, `>>`.white, `Chatbot Used`.red);
         const randomNumber = Math.floor(Math.random() * 30);
         json =
-          randomNumber == 0 ? (json ?? '') + s[0] : randomNumber == 1 ? (json ?? '') + s[1] : json;
+          randomNumber == 0
+            ? (json ?? '') + s[0]
+            : randomNumber == 1
+            ? (json ?? '') + s[1]
+            : json;
         let component: any[] = [];
         if (!guild.chatbotFilter) {
           if (filter.isProfane(json)) {
@@ -285,7 +332,9 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
         });
 
         Influx?.writePoint(
-          new Point('commandruncount').tag('action', 'addition').intField('usage', 1),
+          new Point('commandruncount')
+            .tag('action', 'addition')
+            .intField('usage', 1),
         );
       })
       .catch();
@@ -295,7 +344,10 @@ Use the  \`${guild.Prefix}help\` to see all my commands.`,
   try {
     Schema.findOne(
       { Guild: message.guildId, Channel: message.channelId },
-      async (err: any, data: { LastMessage: any; Content: any; save: () => void }) => {
+      async (
+        err: any,
+        data: { LastMessage: any; Content: any; save: () => void },
+      ) => {
         if (!data) return;
 
         const lastStickyMessage = await client.helpers.getMessage(
