05ac9aefaab2bced84f27550d6fb003ded428384
deepparag <deep.p.sarda@gmail.com>
2023-02-02 15:56:29 +0530

    Update Embed Builder

--- packages/events/src/extras/embedbuilder.ts
+++ packages/events/src/extras/embedbuilder.ts
@@ -72,38 +72,38 @@ export default (client: AeonaBot) => {
         content: defaultContent,
         title: 'Variables for you to use.',
         description: `
-        <:F_txt1:1049288903263277086><:ayyy:1056627813286952980>  **User Variables**
-        <:F_txt2:1049288870199566366> __Variable <:F_Arrow:1049291677359153202> Description <:F_Arrow:1049291677359153202> Example__
-        <:F_txt2:1049288870199566366> \`{user:username}\` <:F_Arrow:1049291677359153202> User's Name <:F_Arrow:1049291677359153202> {user:username}
-        <:F_txt2:1049288870199566366> \`{user:discriminator}\` <:F_Arrow:1049291677359153202> User's Discriminator<:F_Arrow:1049291677359153202> {user:discriminator}
-        <:F_txt2:1049288870199566366> \`{user:tag}\` <:F_Arrow:1049291677359153202> User's Tag<:F_Arrow:1049291677359153202> {user:tag}
-        <:F_txt2:1049288870199566366> \`{user:mention}\` <:F_Arrow:1049291677359153202> User ping<:F_Arrow:1049291677359153202> {user:mention}
-        <:F_txt2:1049288870199566366> \`{user:invites}\` <:F_Arrow:1049291677359153202> Number of users invited<:F_Arrow:1049291677359153202> {user:invites}
-        <:F_txt2:1049288870199566366> \`{user:invites:left}\` <:F_Arrow:1049291677359153202> Number of users left after inviting <:F_Arrow:1049291677359153202> {user:invites:left}
-        <:F_txt2:1049288870199566366> \`{user:level}\` <:F_Arrow:1049291677359153202> User's level<:F_Arrow:1049291677359153202> {user:level}
-        <:F_txt2:1049288870199566366> \`{user:xp}\` <:F_Arrow:1049291677359153202> User's xp <:F_Arrow:1049291677359153202> {user:xp}
-        <:F_txt2:1049288870199566366> \`{user:rank}\` <:F_Arrow:1049291677359153202> User's rank<:F_Arrow:1049291677359153202> {user:rank}
-        <:F_txt2:1049288870199566366> \`{user:avatar}\` <:F_Arrow:1049291677359153202> Link to user's avatar
-        <:F_txt4:1049288828646588547> <:YaeSmug:1062031989714198678> **Inviter Variables** *The user who invited the user*
-        <:F_txt2:1049288870199566366> \`{inviter:username}\` <:F_Arrow:1049291677359153202> User's Name <:F_Arrow:1049291677359153202> {inviter:username}
-        <:F_txt2:1049288870199566366> \`{inviter:discriminator}\` <:F_Arrow:1049291677359153202> User's Discriminator<:F_Arrow:1049291677359153202> {inviter:discriminator}
-        <:F_txt2:1049288870199566366> \`{inviter:tag}\` <:F_Arrow:1049291677359153202> User's Tag <:F_Arrow:1049291677359153202> {inviter:tag}
-        <:F_txt2:1049288870199566366> \`{inviter:mention}\` <:F_Arrow:1049291677359153202> User ping<:F_Arrow:1049291677359153202> {inviter:mention}
-        <:F_txt2:1049288870199566366> \`{inviter:invites}\` <:F_Arrow:1049291677359153202> Number of users invited<:F_Arrow:1049291677359153202> {inviter:invites}
-        <:F_txt2:1049288870199566366> \`{inviter:invites:left}\` <:F_Arrow:1049291677359153202> Number of users left after inviting <:F_Arrow:1049291677359153202> {inviter:invites:left}
-        <:F_txt2:1049288870199566366> \`{inviter:level}\` <:F_Arrow:1049291677359153202> User's level <:F_Arrow:1049291677359153202> {inviter:level}
-        <:F_txt2:1049288870199566366> \`{inviter:xp}\` <:F_Arrow:1049291677359153202> User's xp <:F_Arrow:1049291677359153202> {inviter:xp}
-        <:F_txt2:1049288870199566366> \`{inviter:rank}\` <:F_Arrow:1049291677359153202> User's rank<:F_Arrow:1049291677359153202> {inviter:rank}
-        <:F_txt2:1049288870199566366> \`{inviter:avatar}\` <:F_Arrow:1049291677359153202> Link to user's avatar
-        <:F_txt4:1049288828646588547> <:AH_LoveCat:1050681792060985414> **Server Variables**
-        <:F_txt2:1049288870199566366> \`{guild:name}\` <:F_Arrow:1049291677359153202> Server's Name <:F_Arrow:1049291677359153202> {guild:name}
-        <:F_txt2:1049288870199566366> \`{guild:owner}\` <:F_Arrow:1049291677359153202> Ping to the server's owner <:F_Arrow:1049291677359153202> {guild:owner}
-        <:F_txt2:1049288870199566366> \`{guild:tier}\` <:F_Arrow:1049291677359153202> Server's boosting tier <:F_Arrow:1049291677359153202> {guild:tier}
-        <:F_txt2:1049288870199566366> \`{guild:description}\` <:F_Arrow:1049291677359153202> Server's description <:F_Arrow:1049291677359153202> {guild:description}
-        <:F_txt2:1049288870199566366> \`{guild:boosts}\` <:F_Arrow:1049291677359153202>The number of boosts this server has <:F_Arrow:1049291677359153202>3 {guild:boosts}
-        <:F_txt2:1049288870199566366> \`{guild:rules}\` <:F_Arrow:1049291677359153202> The ping of the channel setup for rules <:F_Arrow:1049291677359153202> {guild:rules}
-        <:F_txt2:1049288870199566366> \`{guild:icon}\` <:F_Arrow:1049291677359153202> Link to server's icon
-        <:F_txt3:1049288938017267773> \`{guild:banner}\` <:F_Arrow:1049291677359153202> Link to server's banner
+        <:ayyy:1056627813286952980> **User Variables**
+         __Variable <:F_Arrow:1049291677359153202> Description <:F_Arrow:1049291677359153202> Example__
+         \`{user:username}\` <:F_Arrow:1049291677359153202> User's Name <:F_Arrow:1049291677359153202> {user:username}
+         \`{user:discriminator}\` <:F_Arrow:1049291677359153202> User's Discriminator<:F_Arrow:1049291677359153202> {user:discriminator}
+         \`{user:tag}\` <:F_Arrow:1049291677359153202> User's Tag<:F_Arrow:1049291677359153202> {user:tag}
+         \`{user:mention}\` <:F_Arrow:1049291677359153202> User ping<:F_Arrow:1049291677359153202> {user:mention}
+         \`{user:invites}\` <:F_Arrow:1049291677359153202> Number of users invited<:F_Arrow:1049291677359153202> {user:invites}
+         \`{user:invites:left}\` <:F_Arrow:1049291677359153202> Number of users left after inviting <:F_Arrow:1049291677359153202> {user:invites:left}
+         \`{user:level}\` <:F_Arrow:1049291677359153202> User's level<:F_Arrow:1049291677359153202> {user:level}
+         \`{user:xp}\` <:F_Arrow:1049291677359153202> User's xp <:F_Arrow:1049291677359153202> {user:xp}
+         \`{user:rank}\` <:F_Arrow:1049291677359153202> User's rank<:F_Arrow:1049291677359153202> {user:rank}
+         \`{user:avatar}\` <:F_Arrow:1049291677359153202> Link to user's avatar
+       <:YaeSmug:1062031989714198678> **Inviter Variables** *The user who invited the user*
+         \`{inviter:username}\` <:F_Arrow:1049291677359153202> User's Name <:F_Arrow:1049291677359153202> {inviter:username}
+         \`{inviter:discriminator}\` <:F_Arrow:1049291677359153202> User's Discriminator<:F_Arrow:1049291677359153202> {inviter:discriminator}
+         \`{inviter:tag}\` <:F_Arrow:1049291677359153202> User's Tag <:F_Arrow:1049291677359153202> {inviter:tag}
+         \`{inviter:mention}\` <:F_Arrow:1049291677359153202> User ping<:F_Arrow:1049291677359153202> {inviter:mention}
+         \`{inviter:invites}\` <:F_Arrow:1049291677359153202> Number of users invited<:F_Arrow:1049291677359153202> {inviter:invites}
+         \`{inviter:invites:left}\` <:F_Arrow:1049291677359153202> Number of users left after inviting <:F_Arrow:1049291677359153202> {inviter:invites:left}
+         \`{inviter:level}\` <:F_Arrow:1049291677359153202> User's level <:F_Arrow:1049291677359153202> {inviter:level}
+         \`{inviter:xp}\` <:F_Arrow:1049291677359153202> User's xp <:F_Arrow:1049291677359153202> {inviter:xp}
+         \`{inviter:rank}\` <:F_Arrow:1049291677359153202> User's rank<:F_Arrow:1049291677359153202> {inviter:rank}
+         \`{inviter:avatar}\` <:F_Arrow:1049291677359153202> Link to user's avatar
+       <:AH_LoveCat:1050681792060985414> **Server Variables**
+         \`{guild:name}\` <:F_Arrow:1049291677359153202> Server's Name <:F_Arrow:1049291677359153202> {guild:name}
+         \`{guild:owner}\` <:F_Arrow:1049291677359153202> Ping to the server's owner <:F_Arrow:1049291677359153202> {guild:owner}
+         \`{guild:tier}\` <:F_Arrow:1049291677359153202> Server's boosting tier <:F_Arrow:1049291677359153202> {guild:tier}
+         \`{guild:description}\` <:F_Arrow:1049291677359153202> Server's description <:F_Arrow:1049291677359153202> {guild:description}
+         \`{guild:boosts}\` <:F_Arrow:1049291677359153202>The number of boosts this server has <:F_Arrow:1049291677359153202>3 {guild:boosts}
+         \`{guild:rules}\` <:F_Arrow:1049291677359153202> The ping of the channel setup for rules <:F_Arrow:1049291677359153202> {guild:rules}
+         \`{guild:icon}\` <:F_Arrow:1049291677359153202> Link to server's icon
+         \`{guild:banner}\` <:F_Arrow:1049291677359153202> Link to server's banner
         `,
       };
     const userData = await fetchData(ctx.user!.id, ctx.guildId!);
