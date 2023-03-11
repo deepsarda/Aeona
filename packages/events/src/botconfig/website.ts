d8157d910866e6a1015d6c56e5aa9a908bb90239
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-03-11 17:11:33 +0530

    bug fixes

--- packages/events/src/botconfig/website.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  anylatics: '',
-  port: process.env.PORT,
-  https: process.env.DEV ? 'http' : 'https',
-  url: process.env.DEV ? 'localhost' : process.env.WEBSITE!.split('://')[1],
-  mongodb_url: process.env.MONGO_CONNECTION,
-};
