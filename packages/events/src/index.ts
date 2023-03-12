175bda773b7c1211c98a2f25cc8040256181dd5c
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-03-12 12:59:56 +0530

    bug fixes

--- packages/events/src/index.ts
+++ packages/events/src/index.ts
@@ -114,6 +114,7 @@ const cachebot = createProxyCache(b, {
 
   setItem: async (table, item) => {
     const t = JSON.stringify(item);
+    if (table == 'guild') console.log(t);
     if (table == 'channel') item = await db.set(`/channel/${item.id}`, t);
     if (table == 'guild') item = await db.set(`/guild/${item.id}`, t);
     if (table == 'user') item = await db.set(`/user/${item.id}`, t);
