f2d7e1851abef02ca1f1215ad4bd1ae20632cd16
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-03-21 11:14:46 +0530

    change rest reset time

--- packages/rest/src/handleRequest.ts
+++ packages/rest/src/handleRequest.ts
@@ -1,27 +1,27 @@
-import { createRestManager } from '@discordeno/rest';
+import { createRestManager } from "@discordeno/rest";
 
-import config from './config.js';
-import runMethod from './requests/runMethod.js';
-import sendRequest from './requests/sendRequest.js';
-import { RequestTypes } from './types.js';
+import config from "./config.js";
+import runMethod from "./requests/runMethod.js";
+import sendRequest from "./requests/sendRequest.js";
+import { RequestTypes } from "./types.js";
 
-const { DISCORD_TOKEN } = config(['DISCORD_TOKEN']);
+const { DISCORD_TOKEN } = config(["DISCORD_TOKEN"]);
 
 let rest = createRestManager({
   token: DISCORD_TOKEN,
 });
 
-//Set a interval for 1 hour
+//Set a interval for 10 minutes
 setInterval(() => {
   rest = createRestManager({
     token: DISCORD_TOKEN,
   });
-}, 3600000);
+}, 60 * 1000 * 10);
 const handleRequest = async (req: RequestTypes): Promise<unknown> => {
   switch (req.type) {
-    case 'RUN_METHOD':
+    case "RUN_METHOD":
       return runMethod(req.data, rest);
-    case 'SEND_REQUEST':
+    case "SEND_REQUEST":
       return sendRequest(req.data, rest);
   }
 };
