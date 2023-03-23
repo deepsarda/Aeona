56556a69fdb4e77b52a3cc8558b64af9f6a2c8f7
deepsarda <deep.p.sarda@gmail.com>
2023-03-23 17:38:05 +0000

    bug fixes

--- src/website/src/config/envs/index.ts
+++ src/website/src/config/envs/index.ts
@@ -1,7 +1,6 @@
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 
 export const envs = {
   ...process.env,
-  ...dotenv.config().parsed
+  ...dotenv.config().parsed,
 };
-export const isProduction = process.env.NODE_ENV === "production";
--- src/website/src/config/logger/index.ts
+++ src/website/src/config/logger/index.ts
@@ -1,25 +1,22 @@
-import { $log, PlatformLoggerSettings } from "@tsed/common";
-import { isProduction } from "../envs/index.js";
+import { $log, PlatformLoggerSettings } from '@tsed/common';
 
-if (isProduction) {
-  $log.appenders.set("stdout", {
-    type: "stdout",
-    levels: ["info", "debug"],
-    layout: {
-      type: "json"
-    }
-  });
+$log.appenders.set('stdout', {
+  type: 'stdout',
+  levels: ['info', 'debug'],
+  layout: {
+    type: 'json',
+  },
+});
 
-  $log.appenders.set("stderr", {
-    levels: ["trace", "fatal", "error", "warn"],
-    type: "stderr",
-    layout: {
-      type: "json"
-    }
-  });
-}
+$log.appenders.set('stderr', {
+  levels: ['trace', 'fatal', 'error', 'warn'],
+  type: 'stderr',
+  layout: {
+    type: 'json',
+  },
+});
 
 export default <PlatformLoggerSettings>{
-  disableRoutesSummary: isProduction,
-  logRequest: false
+  disableRoutesSummary: true,
+  logRequest: false,
 };
