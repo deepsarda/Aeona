dd7804493c1cfab7ba8d08cffaa5c1f5e4ac9b13
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-05-29 19:58:45 +0530

    Update imagine.ts

--- src/commands/image/imagine.ts
+++ src/commands/image/imagine.ts
@@ -516,13 +516,26 @@ export default {
           files: [
             {
               blob: response.response[0],
-              name: 'image1.jpg',
+              name: 'image1.png',
+            },
+             {
+              blob: response.response[1],
+              name: 'image2.png',
+            },
+             {
+              blob: response.response[2],
+              name: 'image3.png',
+            },
+             {
+              blob: response.response[3],
+              name: 'image4.png',
             },
           ],
         });
       });
     } catch (e) {
-      console.log(`Imagine Error: ${e}`);
+      console.log(`Imagine Error:`);
+      console.error(e);
     }
   },
 } as CommandOptions;
