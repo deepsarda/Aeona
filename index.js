6274ef9e067c7c6eaf10d5d53d5fe03732599bcd
tytech039 <107938098+tytech039@users.noreply.github.com>
2023-06-06 22:51:46 -0400

    Update index.js
    Refactor the code.
    Function Extraction: removeDistDirectory and buildAndCopyFiles make readability and future edits easier.
    Async Await: The new functions are defined as Async, so future async operations can occur.
    path.join use: Contencate the parts of paths for copying files.
    Template Strings: In the runBot and buildAndCopyFiles functions, template strings "(`yarn start --id=${id}` and `cp -R ${path.join('src', 'website', 'views')} ${path.join('dist', 'website')}`)" are used instead of string concatenation. This makes the code cleaner and easier to read.
    Main Function: A new main function was added that calls the removeDistDirectory, buildAndCopyFiles, and setInterval functions in their appropriate order.

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 import { exec, execSync } from 'child_process';
+import path from 'path';
 
 console.log('Building Bot...');
-try {
-  execSync('rm -rf dist');
-} catch (e) {
-  console.log(e.toString('ascii').trim());
+
+async function removeDistDirectory() {
   try {
-    execSync('rd /s /q dist');
+    execSync('rm -rf dist');
+  } catch (e) {
+    console.log(e.toString('ascii').trim());
+    try {
+      execSync('rd /s /q dist');
+    } catch (e) {
+      console.log(e.toString('ascii').trim());
+    }
+  }
+}
+
+async function buildAndCopyFiles() {
+  try {
+    console.log(execSync('yarn build').toString('ascii').trim());
+    console.log(execSync(`cp -R ${path.join('src', 'website', 'views')} ${path.join('dist', 'website')}`).toString('ascii').trim());
+    console.log(execSync(`cp -R ${path.join('src', 'website', 'public')} ${path.join('dist', 'website')}`).toString('ascii').trim());
   } catch (e) {
     console.log(e.toString('ascii').trim());
   }
 }
-console.log();
-try {
-  console.log(execSync('yarn build').toString('ascii').trim());
-  console.log(
-    execSync('cp -R src/website/views dist/website/').toString('ascii').trim(),
-  );
-  console.log(
-    execSync('cp -R src/website/public dist/website/').toString('ascii').trim(),
-  );
-} catch (e) {
-  console.log(e.toString('ascii').trim());
-}
 
 function runBot(id) {
-  const ls = exec('yarn start --id=' + id);
+  const ls = exec(`yarn start --id=${id}`);
 
   ls.stdout.on('data', (data) => {
     console.log(data.toString('ascii').trim());
@@ -40,15 +42,23 @@ function runBot(id) {
     runBot(id);
   });
 }
-let ids = ['931226824753700934'];
-ids.forEach((id) => {
-  runBot(id);
-});
 
-setInterval(() => {
-  try {
-    exec('git pull');
-  } catch (e) {
-    console.log(e);
-  }
-}, 60 * 1000);
+async function main() {
+  await removeDistDirectory();
+  await buildAndCopyFiles();
+
+  const ids = ['931226824753700934'];
+  ids.forEach((id) => {
+    runBot(id);
+  });
+
+  setInterval(() => {
+    try {
+      exec('git pull');
+    } catch (e) {
+      console.log(e);
+    }
+  }, 60 * 1000);
+}
+
+main();
