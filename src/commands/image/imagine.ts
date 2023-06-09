d520a8292e2c6713630ac3db6d008092f1bf0537
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-06-09 22:31:57 +0530

    Fix imagine

--- src/commands/image/imagine.ts
+++ src/commands/image/imagine.ts
@@ -1,10 +1,14 @@
-import { CommandOptions, Components, Context } from '@thereallonewolf/amethystframework';
-import Filter from 'badwords-filter';
-import fetch from 'node-fetch';
+import {
+  CommandOptions,
+  Components,
+  Context,
+} from "@thereallonewolf/amethystframework";
+import Filter from "badwords-filter";
+import fetch from "node-fetch";
 
-import GuildDB from '../../database/models/guild.js';
-import { AeonaBot } from '../../extras/index.js';
-import badwords from '../../Collection/badwords.js';
+import GuildDB from "../../database/models/guild.js";
+import { AeonaBot } from "../../extras/index.js";
+import badwords from "../../Collection/badwords.js";
 
 const filter = new Filter({
   list: badwords,
@@ -283,22 +287,24 @@ export default {
 */
 
 async function query(prompt) {
-  const response = await fetch(`http://localhost:8083/chatbot/image?prompt=${prompt}`);
+  const response = await fetch(
+    `http://localhost:8083/chatbot/image?prompt=${prompt}`
+  );
 
   return await response.json();
 }
 
 export default {
-  name: 'imagine',
-  description: 'Generate a image',
-  commandType: ['application', 'message'],
-  category: 'image',
+  name: "imagine",
+  description: "Generate a image",
+  commandType: ["application", "message"],
+  category: "image",
   args: [
     {
-      name: 'prompt',
-      description: 'describe the image you want to generate',
+      name: "prompt",
+      description: "describe the image you want to generate",
       required: true,
-      type: 'String',
+      type: "String",
     },
   ],
   extras: {
@@ -307,117 +313,120 @@ export default {
   async execute(client: AeonaBot, ctx: Context) {
     try {
       if (!ctx.guild || !ctx.user || !ctx.channel) return;
-      const prompt = ctx.options.getLongString('prompt', true);
+      const prompt = ctx.options.getLongString("prompt", true);
       if ([1080026325374476349n].includes(ctx.user.id))
         return ctx.reply({
-          content: 'You have been banned from this command.',
+          content: "You have been banned from this command.",
         });
       let guild = await GuildDB.findOne({ Guild: ctx.guild.id });
       if (!guild)
         guild = new GuildDB({
           Guild: ctx.guild.id,
         });
-      if (!(guild.isPremium === 'true')) {
+      if (!(guild.isPremium === "true")) {
         if (!ctx.channel.nsfw)
           if (filter.isUnclean(prompt))
             return ctx.reply({
-              content: 'This prompt is either profane, nfsw or both.',
+              content: "This prompt is either profane, nfsw or both.",
             });
       }
       const comp = new Components();
-      comp.addSelectComponent('Choose your style', 'style', [
+      comp.addSelectComponent("Choose your style", "style", [
         {
-          label: 'accurate',
-          description: 'Generates the image most accurate to your prompt but will be less amazing.',
-          value: 'accurate',
+          label: "accurate",
+          description:
+            "Generates the image most accurate to your prompt but will be less amazing.",
+          value: "accurate",
         },
         {
-          label: 'dramatic',
-          description: 'Not the most accurate but generates the best images',
-          value: 'dramatic',
+          label: "dramatic",
+          description: "Not the most accurate but generates the best images",
+          value: "dramatic",
         },
         {
-          label: 'portrait',
-          description: 'Best for images of a single living being.',
-          value: 'portrait',
+          label: "portrait",
+          description: "Best for images of a single living being.",
+          value: "portrait",
         },
         {
-          label: 'photo',
-          description: 'One of the most accurate and best for real life images. Prefer this over accurate.',
-          value: 'photo',
+          label: "photo",
+          description:
+            "One of the most accurate and best for real life images. Prefer this over accurate.",
+          value: "photo",
         },
         {
-          label: 'fantasy',
-          description: 'Generate fantasy style images.',
-          value: 'fantasy',
+          label: "fantasy",
+          description: "Generate fantasy style images.",
+          value: "fantasy",
         },
         {
-          label: 'anime',
-          description: 'Make the generated image in the style of a anime',
-          value: 'anime',
+          label: "anime",
+          description: "Make the generated image in the style of a anime",
+          value: "anime",
         },
         {
-          label: 'neo',
-          description: 'Gets some neo led colors for your image.',
-          value: 'neo',
+          label: "neo",
+          description: "Gets some neo led colors for your image.",
+          value: "neo",
         },
         {
-          label: 'cgi',
-          description: 'Make your image like a cgi character.',
-          value: 'cgi',
+          label: "cgi",
+          description: "Make your image like a cgi character.",
+          value: "cgi",
         },
         {
-          label: 'oil painting',
-          description: 'Make your image like a oil painting',
-          value: 'oil',
+          label: "oil painting",
+          description: "Make your image like a oil painting",
+          value: "oil",
         },
         {
-          label: 'horror',
-          description: 'Give your art a horror feeling',
-          value: 'horror',
+          label: "horror",
+          description: "Give your art a horror feeling",
+          value: "horror",
         },
         {
-          label: 'steampunk',
-          description: 'Make your art in a retro style',
-          value: 'steampunk',
+          label: "steampunk",
+          description: "Make your art in a retro style",
+          value: "steampunk",
         },
         {
-          label: 'cyberpunk',
-          description: 'Show the future in your images',
-          value: 'cyberpunk',
+          label: "cyberpunk",
+          description: "Show the future in your images",
+          value: "cyberpunk",
         },
         {
-          label: 'synthwave',
-          description: 'Make your art colorful',
-          value: 'synthwave',
+          label: "synthwave",
+          description: "Make your art colorful",
+          value: "synthwave",
         },
         {
-          label: '3D Game',
-          description: 'Make your image look like a 3D game',
-          value: '3d',
+          label: "3D Game",
+          description: "Make your image look like a 3D game",
+          value: "3d",
         },
         {
-          label: 'epic',
-          description: 'Set your image in a epic style',
-          value: 'epic',
+          label: "epic",
+          description: "Set your image in a epic style",
+          value: "epic",
         },
         {
-          label: 'comic',
-          description: 'Make your image in a comic strip style does not work all the time!',
-          value: 'comic',
+          label: "comic",
+          description:
+            "Make your image in a comic strip style does not work all the time!",
+          value: "comic",
         },
         {
-          label: 'charcoal',
-          description: 'Make the image in charcoal',
-          value: 'charcoal',
+          label: "charcoal",
+          description: "Make the image in charcoal",
+          value: "charcoal",
         },
       ]);
       let modifiers =
-        ' detailed matte painting, deep color, fantastical, intricate detail, splash screen, complementary colors, fantasy concept art, 8k resolution trending on Artstation Unreal Engine 5';
+        " detailed matte painting, deep color, fantastical, intricate detail, splash screen, complementary colors, fantasy concept art, 8k resolution trending on Artstation Unreal Engine 5";
 
       const msg = (
         await ctx.reply({
-          content: 'Choose your style...',
+          content: "Choose your style...",
           components: comp,
           messageReference: {
             messageId: ctx.message?.id,
@@ -430,98 +439,104 @@ export default {
         filter: (bot, data) => data.user.id == ctx.author?.id,
       });
       await client.helpers.editMessage(ctx.channel.id, msg.id, {
-        content: 'GENERATING.... \n Have you tried +quote yet?',
+        content: "GENERATING.... \n Have you tried +quote yet?",
         components: [],
       });
       switch (c.data!.values![0]) {
-        case 'accurate':
-          modifiers = '';
+        case "accurate":
+          modifiers = "";
           break;
-        case 'portrait':
+        case "portrait":
           modifiers =
-            ' head and shoulders portrait, 8k resolution concept art portrait by Greg Rutkowski, Artgerm, WLOP, Alphonse Mucha dynamic lighting hyperdetailed intricately detailed Splash art trending on Artstation triadic colors Unreal Engine 5 volumetric lighting';
+            " head and shoulders portrait, 8k resolution concept art portrait by Greg Rutkowski, Artgerm, WLOP, Alphonse Mucha dynamic lighting hyperdetailed intricately detailed Splash art trending on Artstation triadic colors Unreal Engine 5 volumetric lighting";
           break;
-        case 'photo':
-          modifiers = ' Professional photography, bokeh, natural lighting, megapixels sharp focus';
-          break;
-        case 'fantasy':
+        case "photo":
           modifiers =
-            ' a masterpiece, 8k resolution, dark fantasy concept art, by Greg Rutkowski, dynamic lighting, hyperdetailed, intricately detailed, Splash screen art, trending on Artstation, deep color, Unreal Engine, volumetric lighting, Alphonse Mucha, Jordan Grimmer, purple and yellow complementary colours';
+            " Professional photography, bokeh, natural lighting, megapixels sharp focus";
           break;
-        case 'anime':
+        case "fantasy":
           modifiers =
-            ' Studio Ghibli, Anime Key Visual, by Makoto Shinkai, Deep Color, Intricate, 8k resolution concept art, Natural Lighting, Beautiful Composition';
+            " a masterpiece, 8k resolution, dark fantasy concept art, by Greg Rutkowski, dynamic lighting, hyperdetailed, intricately detailed, Splash screen art, trending on Artstation, deep color, Unreal Engine, volumetric lighting, Alphonse Mucha, Jordan Grimmer, purple and yellow complementary colours";
           break;
-        case 'neo':
+        case "anime":
           modifiers =
-            ' neo-impressionism expressionist style oil painting, smooth post-impressionist impasto acrylic painting, thick layers of colourful textured paint';
+            " Studio Ghibli, Anime Key Visual, by Makoto Shinkai, Deep Color, Intricate, 8k resolution concept art, Natural Lighting, Beautiful Composition";
           break;
-        case 'oil':
-          modifiers = ' oil painting by James Gurney';
-          break;
-        case 'horror':
-          modifiers = ' horror Gustave Doré Greg Rutkowski';
-          break;
-        case 'steampunk':
-          modifiers = ' steampunk engine';
-          break;
-        case 'cyberpunk':
-          modifiers = ' cyberpunk 2099 blade runner 2049 neon';
-          break;
-        case 'synthwave':
-          modifiers = ' synthwave neon retro';
-          break;
-        case '3d':
-          modifiers = ' trending on Artstation Unreal Engine 3D shading shadow depth';
-          break;
-        case 'epic':
+        case "neo":
           modifiers =
-            ' Epic cinematic brilliant stunning intricate meticulously detailed dramatic atmospheric maximalist digital matte painting';
+            " neo-impressionism expressionist style oil painting, smooth post-impressionist impasto acrylic painting, thick layers of colourful textured paint";
           break;
-        case 'comic':
-          modifiers = ' Mark Brooks and Dan Mumford, comic book art, perfect, smooth';
+        case "oil":
+          modifiers = " oil painting by James Gurney";
           break;
-        case 'charcoal':
-          modifiers = ' hyperdetailed charcoal drawing';
+        case "horror":
+          modifiers = " horror Gustave Doré Greg Rutkowski";
+          break;
+        case "steampunk":
+          modifiers = " steampunk engine";
+          break;
+        case "cyberpunk":
+          modifiers = " cyberpunk 2099 blade runner 2049 neon";
+          break;
+        case "synthwave":
+          modifiers = " synthwave neon retro";
+          break;
+        case "3d":
+          modifiers =
+            " trending on Artstation Unreal Engine 3D shading shadow depth";
+          break;
+        case "epic":
+          modifiers =
+            " Epic cinematic brilliant stunning intricate meticulously detailed dramatic atmospheric maximalist digital matte painting";
+          break;
+        case "comic":
+          modifiers =
+            " Mark Brooks and Dan Mumford, comic book art, perfect, smooth";
+          break;
+        case "charcoal":
+          modifiers = " hyperdetailed charcoal drawing";
           break;
       }
 
       query(`${prompt},${modifiers}`).then(async (response: any) => {
-        client.helpers.sendMessage('1044575489118978068', {
-          content: `**User:**${ctx.user!.id} ${ctx.user!.username}#${ctx.user!.discriminator} \n **Guild:** ${
-            ctx.guild!.name
-          } ${ctx.guildId} \n**Prompt:** ${prompt}\n **Mode:** ${c.data?.values![0]}`,
+        client.helpers.sendMessage("1044575489118978068", {
+          content: `**User:**${ctx.user!.id} ${ctx.user!.username}#${
+            ctx.user!.discriminator
+          } \n **Guild:** ${ctx.guild!.name} ${
+            ctx.guildId
+          } \n**Prompt:** ${prompt}\n **Mode:** ${c.data?.values![0]}`,
           files: client.extras.findFiles([
             {
               blob: response.response[0],
-              name: 'image1.png',
+              name: "image1.png",
             },
             {
               blob: response.response[1],
-              name: 'image2.png',
+              name: "image2.png",
             },
             {
               blob: response.response[2],
-              name: 'image3.png',
+              name: "image3.png",
             },
           ]),
         });
 
         ctx.editReply({
           content: `**Prompt:** ${prompt}\n **Mode:** ${c.data?.values![0]}`,
+        });
+        ctx.reply({
           files: client.extras.findFiles([
             {
               blob: response.response[0],
-              name: 'image1.png',
+              name: "image1.png",
             },
           ]),
         });
-
         ctx.reply({
           files: client.extras.findFiles([
             {
               blob: response.response[1],
-              name: 'image3.png',
+              name: "image2.png",
             },
           ]),
         });
@@ -529,7 +544,7 @@ export default {
           files: client.extras.findFiles([
             {
               blob: response.response[2],
-              name: 'image2.png',
+              name: "image3.png",
             },
           ]),
         });
