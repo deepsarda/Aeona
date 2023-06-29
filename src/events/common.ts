a714282ac4aea6b1c9ed9073f5e473e05587fa8f
deepsarda <deep.p.sarda@gmail.com>
2023-06-29 08:12:50 +0000

    Fixes

--- src/events/common.ts
+++ src/events/common.ts
@@ -1,11 +1,9 @@
-import type { ArgsOf } from "discordx";
-import { Discord, On } from "discordx";
-import { AeonaBot } from "../utils/types";
+import type { ArgsOf } from 'discordx';
+import { Discord, On } from 'discordx';
+import { AeonaBot } from '../utils/types';
 
 @Discord()
 export class Common {
   @On()
-  messageDelete([message]: ArgsOf<"messageDelete">, client: AeonaBot): void {
-    console.log("Message Deleted", client.user?.username, message.content);
-  }
+  messageDelete([message]: ArgsOf<'messageDelete'>, client: AeonaBot): void {}
 }
