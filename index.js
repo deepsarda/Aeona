78a412c898c835ab97c630c52d4b67b3ce35630c
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-06-29 16:23:12 +0530

    Improvements

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
-import { exec, execSync } from 'child_process';
-
-console.log('Building Bot...');
-
-async function removeBuildDirectory() {
-  try {
-    execSync('rm -rf build');
-  } catch (e) {
-    console.log(e.toString('ascii').trim());
-    try {
-      execSync('rd /s /q build');
-    } catch (e) {
-      console.log(e.toString('ascii').trim());
-    }
-  }
-}
-
-async function build() {
-  try {
-    await removeBuildDirectory();
-    console.log(execSync('yarn build').toString('ascii').trim());
-  } catch (e) {
-    console.log(e.toString('ascii').trim());
-  }
-}
+import { exec, execSync } from "child_process";
 
 function runBot() {
   const ls = exec(`yarn start`);
 
-  ls.stdout.on('data', (data) => {
-    console.log(data.toString('ascii').trim());
+  ls.stdout.on("data", (data) => {
+    console.log(data.toString("ascii").trim());
   });
 
-  ls.stderr.on('data', (data) => {
-    console.error(data.toString('ascii').trim());
+  ls.stderr.on("data", (data) => {
+    console.error(data.toString("ascii").trim());
   });
 
-  ls.on('close', (code) => {
+  ls.on("close", (code) => {
     console.log(`child process exited with code ${code}`);
     setTimeout(() => {
       runBot();
@@ -44,13 +20,11 @@ function runBot() {
 }
 
 async function main() {
-  await build();
-
   runBot();
 
   setInterval(() => {
     try {
-      exec('git pull');
+      exec("git pull");
     } catch (e) {
       console.log(e);
     }
