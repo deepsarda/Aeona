1f4e3bc6e40c24e2021fed7602b73a3193e1786b
deepsarda <deep.p.sarda@gmail.com>
2023-07-31 13:46:07 +0000

    Fix moderation

--- src/plugins/moderation/message.ts
+++ src/plugins/moderation/message.ts
@@ -1,7 +1,7 @@
 import { Category, RateLimit, TIME_UNIT } from '@discordx/utilities';
 import { Bot, Guard, SimpleCommandMessage } from 'discordx';
 import { Discord, SimpleCommand, SimpleCommandOption, SimpleCommandOptionType } from 'discordx';
-import hmfull from 'hmfull';
+
 import { bot } from '../../bot.js';
 import { getPluginsBot } from '../../utils/config.js';
 import { GuildMember, User } from 'discord.js';
@@ -15,69 +15,69 @@ import { GuildMember, User } from 'discord.js';
   }),
 )
 export class Moderation {
-    @SimpleCommand({
-        name: "kick",
-        description: "Kick a member out of the server!"
+  @SimpleCommand({
+    name: 'kick',
+    description: 'Kick a member out of the server!',
+  })
+  async kick(
+    @SimpleCommandOption({
+      name: 'member',
+      description: 'The member to be kicked',
+      type: SimpleCommandOptionType.User,
     })
-    async kick(
-        @SimpleCommandOption({
-            name: "member",
-            description: "The member to be kicked",
-            type: SimpleCommandOption.User
-        })
-        member: User | GuildMember | undefined,
-        @SimpleCommandOption({
-            name: 'reason',
-            description: 'Why you want to kick the member',
-            type: SimpleCommandOptionType.String,
-          })
-          reason: string = 'No reason given!',
-          command: SimpleCommandMessage,
-    ) {
-        const ctx = command.message;
-
-        if (!member) return bot.extras.errUsage({ usage: '+kick @Member [reason]' }, command);
-
-        bot.extras.embed(
-            {
-                title: `${ctx.author.username} was kicked out of the server!`,
-                description: `Member: ${ctx.author}\nAction: Kicked\nReason: ${reason}`,
-                type: 'reply',
-            },
-            command
-        );
-    }
-
-    @SimpleCommand({
-        name: "ban",
-        description: "Ban a member from the server!"
+    member: User | GuildMember | undefined,
+    @SimpleCommandOption({
+      name: 'reason',
+      description: 'Why you want to kick the member',
+      type: SimpleCommandOptionType.String,
     })
-    async ban(
-        @SimpleCommandOption({
-            name: "member",
-            description: "The member to be banned",
-            type: SimpleCommandOption.User
-        })
-        member: User | GuildMember | undefined,
-        @SimpleCommandOption({
-            name: 'reason',
-            description: 'Why you want to ban the member',
-            type: SimpleCommandOptionType.String,
-          })
-          reason: string = 'No reason given!',
-          command: SimpleCommandMessage,
-    ) {
-        const ctx = command.message;
+    reason: string = 'No reason given!',
+    command: SimpleCommandMessage,
+  ) {
+    const ctx = command.message;
 
-        if (!member) return bot.extras.errUsage({ usage: '+ban @Member [reason]' }, command);
+    if (!member) return bot.extras.errUsage({ usage: '+kick @Member [reason]' }, command);
 
-        bot.extras.embed(
-            {
-                title: `${ctx.author.username} was banned from the server!`,
-                description: `Member: ${ctx.author}\nAction: Banned\nReason: ${reason}`,
-                type: 'reply',
-            },
-            command
-        );
-    }
+    bot.extras.embed(
+      {
+        title: `${ctx.author.username} was kicked out of the server!`,
+        desc: `Member: ${ctx.author}\nAction: Kicked\nReason: ${reason}`,
+        type: 'reply',
+      },
+      command,
+    );
+  }
+
+  @SimpleCommand({
+    name: 'ban',
+    description: 'Ban a member from the server!',
+  })
+  async ban(
+    @SimpleCommandOption({
+      name: 'member',
+      description: 'The member to be banned',
+      type: SimpleCommandOptionType.User,
+    })
+    member: User | GuildMember | undefined,
+    @SimpleCommandOption({
+      name: 'reason',
+      description: 'Why you want to ban the member',
+      type: SimpleCommandOptionType.String,
+    })
+    reason: string = 'No reason given!',
+    command: SimpleCommandMessage,
+  ) {
+    const ctx = command.message;
+
+    if (!member) return bot.extras.errUsage({ usage: '+ban @Member [reason]' }, command);
+
+    bot.extras.embed(
+      {
+        title: `${ctx.author.username} was banned from the server!`,
+        desc: `Member: ${ctx.author}\nAction: Banned\nReason: ${reason}`,
+        type: 'reply',
+      },
+      command,
+    );
+  }
 }
