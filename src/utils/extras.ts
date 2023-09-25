a17fd61de40bf79c6702d6e3ed3b0705a84b848c
deepsarda <deep.p.sarda@gmail.com>
2023-09-25 14:41:46 +0000

    Fix bot

--- src/utils/extras.ts
+++ src/utils/extras.ts
@@ -90,10 +90,11 @@ export function additionalProps(client: AeonaBot) {
     ): Promise<(mongoose.Document<any, {}, T> & T) | undefined> => {
       const channels = await schema.find({ Guild: guildId });
       const isPremium = await client.extras.isPremium(guildId);
-      if (!channels || channels.length == 0) return undefined;
-      if (isPremium) return channels.find((channel: any) => channel.Channel === channelId);
+      if (channels.length == 0) return undefined;
+
+      if (isPremium) return channels.find((channel: any) => channel.Channel.trim() == channelId.trim());
       //@ts-ignore
-      else channels[0].Channel == channelId ? channels[0] : undefined;
+      else if (channels[0].Channel.trim() == channelId.trim()) return channels[0];
 
       return undefined;
     },
