d19805b04b6b25906ae0a195fafc7ca33aa1d9b9
deepsarda <deep.p.sarda@gmail.com>
2023-10-01 10:50:20 +0000

    Disable dashboard log

--- src/index.ts
+++ src/index.ts
@@ -56,11 +56,12 @@ for (const config of configs) {
     cluster.on('message', async (msg: any) => {
       if (msg.log) {
         console.log(`[${config.name}]:`.yellow + msg.log);
-        fetch('https://dashboard.aeonabot.xyz/stats/logs/update', {
+       /* fetch('https://dashboard.aeonabot.xyz/stats/logs/update', {
           method: 'post',
           body: `[${config.name}]:`.yellow + msg.log,
           headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + process.env.apiKey },
         }).catch((err) => console.log(err));
+        */
       }
     });
   });
