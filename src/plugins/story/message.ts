4baad62a2061c55bab5ccd9eee7db3013111225c
deepsarda <92147339+deepsarda@users.noreply.github.com>
2023-12-30 12:51:10 +0530

    Bug fixes and improvements

--- src/plugins/story/message.ts
+++ src/plugins/story/message.ts
@@ -1,37 +1,42 @@
-import { Category, RateLimit, TIME_UNIT } from '@discordx/utilities';
-import { Bot, ButtonComponent, Guard, SimpleCommandMessage } from 'discordx';
-import { Discord, SimpleCommand, SimpleCommandOption, SimpleCommandOptionType } from 'discordx';
-import GuildDB from '../../database/models/guild.js';
-import { bot } from '../../bot.js';
-import { getPluginsBot } from '../../utils/config.js';
-import filter from 'leo-profanity';
+import { Category, RateLimit, TIME_UNIT } from "@discordx/utilities";
+import { Bot, ButtonComponent, Guard, SimpleCommandMessage } from "discordx";
+import {
+  Discord,
+  SimpleCommand,
+  SimpleCommandOption,
+  SimpleCommandOptionType,
+} from "discordx";
+import GuildDB from "../../database/models/guild.js";
+import { bot } from "../../bot.js";
+import { getPluginsBot } from "../../utils/config.js";
+import filter from "leo-profanity";
 
-import { AttachmentBuilder, ButtonInteraction } from 'discord.js';
-filter.loadDictionary('en');
-import Topgg from '@top-gg/sdk';
-import { Components } from '../../utils/components.js';
+import { AttachmentBuilder, ButtonInteraction } from "discord.js";
+filter.loadDictionary("en");
+import Topgg from "@top-gg/sdk";
+import { Components } from "../../utils/components.js";
 const api = new Topgg.Api(process.env.TOPGG_TOKEN!);
 @Discord()
-@Bot(...getPluginsBot('story'))
-@Category('story')
+@Bot(...getPluginsBot("story"))
+@Category("story")
 @Guard(
   RateLimit(TIME_UNIT.seconds, 30, {
     rateValue: 3,
-  }),
+  })
 )
 export class Story {
   @SimpleCommand({
-    name: 'story generate',
-    description: 'Create a AI generated story 🖌️',
+    name: "story generate",
+    description: "Create a AI generated story 🖌️",
   })
   async imagine(
     @SimpleCommandOption({
-      name: 'prompt',
-      description: 'What to write a story about?',
+      name: "prompt",
+      description: "What to write a story about?",
       type: SimpleCommandOptionType.String,
     })
     prompt: string | undefined,
-    command: SimpleCommandMessage,
+    command: SimpleCommandMessage
   ) {
     const ctx = command.message;
     prompt = command.argString;
@@ -40,9 +45,9 @@ export class Story {
     if (!prompt)
       return bot.extras.errUsage(
         {
-          usage: '+story generate <prompt>',
+          usage: "+story generate <prompt>",
         },
-        command,
+        command
       );
 
     let guild = await GuildDB.findOne({ Guild: ctx.guild.id });
@@ -50,58 +55,70 @@ export class Story {
       guild = new GuildDB({
         Guild: ctx.guild.id,
       });
-    if (!(guild.isPremium === 'true')) {
+    if (!(guild.isPremium === "true")) {
       if (!(await api.hasVoted(ctx.author.id)))
         return ctx.reply({
           content:
-            'Please **[vote](https://top.gg/bot/931226824753700934/vote)** for me to access this command and then try again. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)',
+            "Please **[vote](https://top.gg/bot/931226824753700934/vote)** for me to access this command and then try again. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)",
 
           components: new Components()
-            .addButton('Upvote', 'Link', 'https://top.gg/bot/931226824753700934/vote')
-            .addButton('Premium', 'Link', 'https://aeonabot.xyz/premium')
-            .addButton('Support', 'Link', 'https://discord.gg/W8hssA32C9'),
+            .addButton(
+              "Upvote",
+              "Link",
+              "https://top.gg/bot/931226824753700934/vote"
+            )
+            .addButton("Premium", "Link", "https://aeonabot.xyz/premium")
+            .addButton("Support", "Link", "https://discord.gg/W8hssA32C9"),
         });
       if (filter.check(prompt))
         return ctx.reply({
           content:
-            'This prompt is either profane, nfsw or both. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)',
+            "This prompt is either profane, nfsw or both. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)",
           components: new Components()
-            .addButton('Premium', 'Link', 'https://aeonabot.xyz/premium')
-            .addButton('Support', 'Link', 'https://discord.gg/W8hssA32C9'),
+            .addButton("Premium", "Link", "https://aeonabot.xyz/premium")
+            .addButton("Support", "Link", "https://discord.gg/W8hssA32C9"),
         });
     }
     const id = ctx.id;
     const comp = new Components();
-    comp.addButton('Choice 1', 'Secondary', 'generate-' + id + '-1');
-    comp.addButton('Choice 2', 'Secondary', 'generate-' + id + '-2');
-    comp.addButton('Choice 3', 'Secondary', 'generate-' + id + '-3');
-    comp.addButton('Choice 4', 'Secondary', 'generate-' + id + '-4');
+    comp.addButton("Choice 1", "Secondary", "generate-" + id + "-1");
+    comp.addButton("Choice 2", "Secondary", "generate-" + id + "-2");
+    comp.addButton("Choice 3", "Secondary", "generate-" + id + "-3");
+    comp.addButton("Choice 4", "Secondary", "generate-" + id + "-4");
 
     ctx.reply({
-      content: 'Generating...',
+      content: "Generating...",
     });
     try {
       while (true) {
-        const response = await (await fetch(`http://localhost:8083/chatbot/story?id=${id}&text=${prompt}`)).json();
+        const response = await (
+          await fetch(
+            `http://localhost:8083/chatbot/story?id=${id}&text=${prompt}`
+          )
+        ).json();
 
         let image;
         try {
-          image = new AttachmentBuilder(Buffer.from(response.image, 'base64'), {
-            name: 'image0.png',
-          });
+          image = new AttachmentBuilder(
+            Buffer.from(response.image.split(",")[1], "base64"),
+            {
+              name: "image0.png",
+            }
+          );
         } catch {
-          console.log('error with image');
+          console.log("error with image");
         }
 
-        const messageContent = response.story + '\n\n\n' + response.options.join('\n');
-        const paragraphs = messageContent.split('\n\n');
-        const contents: string[] = [''];
+        const messageContent =
+          response.story + "\n\n\n" + response.options.join("\n");
+        const paragraphs = messageContent.split("\n\n");
+        const contents: string[] = [""];
 
         for (let i = 0; i < paragraphs.length; i++) {
           const paragraph = paragraphs[i];
           const content = contents[contents.length - 1];
 
-          const text = content + '\n\n' + paragraph;
+          const text = content + "\n\n" + paragraph;
           if (text.length > 1500) contents.push(paragraph);
           else contents[contents.length - 1] = text;
         }
@@ -148,34 +165,42 @@ export class Story {
   })
   async generate(ctx: ButtonInteraction) {
     ctx.reply({
-      content: 'Generating...',
+      content: "Generating...",
     });
-    const ids = ctx.customId.split('-');
+    const ids = ctx.customId.split("-");
     const comp = new Components();
-    comp.addButton('Choice 1', 'Secondary', 'generate-' + ids[1] + '-1');
-    comp.addButton('Choice 2', 'Secondary', 'generate-' + ids[1] + '-2');
-    comp.addButton('Choice 3', 'Secondary', 'generate-' + ids[1] + '-3');
-    comp.addButton('Choice 4', 'Secondary', 'generate-' + ids[1] + '-4');
+    comp.addButton("Choice 1", "Secondary", "generate-" + ids[1] + "-1");
+    comp.addButton("Choice 2", "Secondary", "generate-" + ids[1] + "-2");
+    comp.addButton("Choice 3", "Secondary", "generate-" + ids[1] + "-3");
+    comp.addButton("Choice 4", "Secondary", "generate-" + ids[1] + "-4");
 
-    const response = await (await fetch(`http://localhost:8083/chatbot/story?id=${ids[1]}&text=${ids[2]}`)).json();
+    const response = await (
+      await fetch(
+        `http://localhost:8083/chatbot/story?id=${ids[1]}&text=${ids[2]}`
+      )
+    ).json();
 
     let image;
     try {
-      image = new AttachmentBuilder(Buffer.from(response.image, 'base64'), {
-        name: 'image0.png',
-      });
+      image = new AttachmentBuilder(
+        Buffer.from(response.image.split(",")[1], "base64"),
+        {
+          name: "image0.png",
+        }
+      );
     } catch {
-      console.log('error with image');
+      console.log("error with image");
     }
-    const messageContent = response.story + '\n\n\n' + response.options.join('\n');
-    const paragraphs = messageContent.split('\n\n');
-    const contents: string[] = [''];
+    const messageContent =
+      response.story + "\n\n\n" + response.options.join("\n");
+    const paragraphs = messageContent.split("\n\n");
+    const contents: string[] = [""];
 
     for (let i = 0; i < paragraphs.length; i++) {
       const paragraph = paragraphs[i];
       const content = contents[contents.length - 1];
 
-      const text = content + '\n\n' + paragraph;
+      const text = content + "\n\n" + paragraph;
       if (text.length > 1500) contents.push(paragraph);
       else contents[contents.length - 1] = text;
     }
--- src/plugins/story/slash.ts
+++ src/plugins/story/slash.ts
@@ -1,52 +1,56 @@
-import { RateLimit, TIME_UNIT } from '@discordx/utilities';
-import { Bot, Guard, SlashOption, SlashGroup } from 'discordx';
-import { Discord, Slash } from 'discordx';
-import GuildDB from '../../database/models/guild.js';
-import { bot } from '../../bot.js';
-import { getPluginsBot } from '../../utils/config.js';
-import filter from 'leo-profanity';
-import { ApplicationCommandOptionType, AttachmentBuilder, CommandInteraction } from 'discord.js';
-filter.loadDictionary('en');
+import { RateLimit, TIME_UNIT } from "@discordx/utilities";
+import { Bot, Guard, SlashOption, SlashGroup } from "discordx";
+import { Discord, Slash } from "discordx";
+import GuildDB from "../../database/models/guild.js";
+import { bot } from "../../bot.js";
+import { getPluginsBot } from "../../utils/config.js";
+import filter from "leo-profanity";
+import {
+  ApplicationCommandOptionType,
+  AttachmentBuilder,
+  CommandInteraction,
+} from "discord.js";
+filter.loadDictionary("en");
 
-import Topgg from '@top-gg/sdk';
-import { Components } from '../../utils/components.js';
+import Topgg from "@top-gg/sdk";
+import { Components } from "../../utils/components.js";
 const api = new Topgg.Api(process.env.TOPGG_TOKEN!);
 
 @Discord()
-@Bot(...getPluginsBot('story'))
+@Bot(...getPluginsBot("story"))
 @SlashGroup({
-  name: 'story',
-  description: 'Create a amazing AI generated Stories 🖌️',
+  name: "story",
+  description: "Create a amazing AI generated Stories 🖌️",
 })
-@SlashGroup('story')
+@SlashGroup("story")
 @Guard(
   RateLimit(TIME_UNIT.seconds, 30, {
     rateValue: 3,
-  }),
+  })
 )
 export class Story {
   @Slash({
-    name: 'generate',
-    description: 'Create a beautiful AI generated story 🖌️',
+    name: "generate",
+    description: "Create a beautiful AI generated story 🖌️",
   })
   async generate(
     @SlashOption({
-      name: 'prompt',
-      description: 'What to write a story about?',
+      name: "prompt",
+      description: "What to write a story about?",
       type: ApplicationCommandOptionType.String,
       required: true,
     })
     prompt: string | undefined,
-    command: CommandInteraction,
+    command: CommandInteraction
   ) {
     if (!command.guild) return;
 
     if (!prompt)
       return bot.extras.errUsage(
         {
-          usage: '+story generate <prompt>',
+          usage: "+story generate <prompt>",
         },
-        command,
+        command
       );
 
     let guild = await GuildDB.findOne({ Guild: command.guild.id });
@@ -54,64 +58,76 @@ export class Story {
       guild = new GuildDB({
         Guild: command.guild.id,
       });
-    if (!(guild.isPremium === 'true')) {
+    if (!(guild.isPremium === "true")) {
       if (!(await api.hasVoted(command.user.id)))
         return command.reply({
           content:
-            'Please **[vote](https://top.gg/bot/931226824753700934/vote)** for me to access this command and then try again. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)',
+            "Please **[vote](https://top.gg/bot/931226824753700934/vote)** for me to access this command and then try again. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)",
 
           components: new Components()
-            .addButton('Upvote', 'Link', 'https://top.gg/bot/931226824753700934/vote')
-            .addButton('Premium', 'Link', 'https://aeonabot.xyz/premium')
-            .addButton('Support', 'Link', 'https://discord.gg/W8hssA32C9'),
+            .addButton(
+              "Upvote",
+              "Link",
+              "https://top.gg/bot/931226824753700934/vote"
+            )
+            .addButton("Premium", "Link", "https://aeonabot.xyz/premium")
+            .addButton("Support", "Link", "https://discord.gg/W8hssA32C9"),
         });
       if (filter.check(prompt))
         return command.reply({
           content:
-            'This prompt is either profane, nfsw or both. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)',
+            "This prompt is either profane, nfsw or both. \n\n **Premium servers can bypass this**\n You can get premium for just **$2.99** [here](https://www.patreon.com/aeonapatreon) \n Or \n **Boost** my support [server](https://aeonabot.xyz/support)",
           components: new Components()
-            .addButton('Premium', 'Link', 'https://aeonabot.xyz/premium')
-            .addButton('Support', 'Link', 'https://discord.gg/W8hssA32C9'),
+            .addButton("Premium", "Link", "https://aeonabot.xyz/premium")
+            .addButton("Support", "Link", "https://discord.gg/W8hssA32C9"),
         });
     }
     const id = command.id;
     const comp = new Components();
-    comp.addButton('Choice 1', 'Secondary', 'generate-' + id + '-1');
-    comp.addButton('Choice 2', 'Secondary', 'generate-' + id + '-2');
-    comp.addButton('Choice 3', 'Secondary', 'generate-' + id + '-3');
-    comp.addButton('Choice 4', 'Secondary', 'generate-' + id + '-4');
+    comp.addButton("Choice 1", "Secondary", "generate-" + id + "-1");
+    comp.addButton("Choice 2", "Secondary", "generate-" + id + "-2");
+    comp.addButton("Choice 3", "Secondary", "generate-" + id + "-3");
+    comp.addButton("Choice 4", "Secondary", "generate-" + id + "-4");
     command.reply({
-      content: 'Generating...',
+      content: "Generating...",
     });
     try {
       while (true) {
-        const response = await (await fetch(`http://localhost:8083/chatbot/story?id=${id}&text=${prompt}`)).json();
+        const response = await (
+          await fetch(
+            `http://localhost:8083/chatbot/story?id=${id}&text=${prompt}`
+          )
+        ).json();
         let image;
         try {
-          image = new AttachmentBuilder(Buffer.from(response.image, 'base64'), {
-            name: 'image0.png',
+          image = new AttachmentBuilder(Buffer.from(response.image, "base64"), {
+            name: "image0.png",
           });
         } catch {
-          console.log('error with image');
+          console.log("error with image");
           try {
-            console.log(Buffer.from(response.image, 'base64'));
-            image = new AttachmentBuilder(Buffer.from(response.image, 'base64'), {
-              name: 'image0.png',
-            });
+            console.log(Buffer.from(response.image.split(",")[1], "base64"));
+            image = new AttachmentBuilder(
+              Buffer.from(response.image.split(",")[1], "base64"),
+              {
+                name: "image0.png",
+              }
+            );
           } catch (e) {
             console.error(e);
           }
         }
 
-        const messageContent = response.story + '\n\n\n' + response.options.join('\n');
-        const paragraphs = messageContent.split('\n\n');
-        const contents: string[] = [''];
+        const messageContent =
+          response.story + "\n\n\n" + response.options.join("\n");
+        const paragraphs = messageContent.split("\n\n");
+        const contents: string[] = [""];
 
         for (let i = 0; i < paragraphs.length; i++) {
           const paragraph = paragraphs[i];
           const content = contents[contents.length - 1];
 
-          const text = content + '\n\n' + paragraph;
+          const text = content + "\n\n" + paragraph;
           if (text.length > 1500) contents.push(paragraph);
           else contents[contents.length - 1] = text;
         }
