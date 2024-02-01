683ea2f187fcf70b5db5ff65bd8d273d27eb3343
deepsarda <deep.p.sarda@gmail.com>
2024-02-01 16:23:09 +0000

    Fix birthdays

--- src/database/models/birthday.ts
+++ src/database/models/birthday.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Schema = new mongoose.Schema({
-  Guild: { type: String, required: true },
+  Guild: { type: String, required: false, default: '' },
   User: { type: String, required: true },
   Birthday: { type: String, required: true },
   Date: { type: String, required: true },
